c28a9a722453d7ed1c063ae10c665e35d0a3130f
Ian Bicking <ianb@colorstudy.com>
2007-09-14 16:00:14 -0500

    [svn r2922] Add setuptools installation

--- refresh-support-files.py
+++ refresh-support-files.py
@@ -4,21 +4,25 @@ Refresh any files in support-files/ that come from elsewhere
 
 import os
 import urllib
+import sys
 
 here = os.path.dirname(__file__)
 support_files = os.path.join(here, 'support-files')
 
 files = [
     ('http://peak.telecommunity.com/dist/ez_setup.py', 'ez_setup.py'),
+    ('http://pypi.python.org/packages/2.5/s/setuptools/setuptools-0.6c7-py2.5.egg', 'setuptools-0.6c7-py2.5.egg'),
+    ('http://pypi.python.org/packages/2.4/s/setuptools/setuptools-0.6c7-py2.4.egg', 'setuptools-0.6c7-py2.4.egg'),
     ]
 
 def main():
     for url, filename in files:
-        print 'fetching', url,
+        print 'fetching', url, '...',
+        sys.stdout.flush()
         f = urllib.urlopen(url)
-        print 'done.'
         content = f.read()
         f.close()
+        print 'done.'
         filename = os.path.join(support_files, filename)
         if os.path.exists(filename):
             f = open(filename, 'rb')
