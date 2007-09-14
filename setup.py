823323e3f2ce69f45c2ee92801af2c21ff43bf93
Ian Bicking <ianb@colorstudy.com>
2007-09-14 18:30:39 -0500

    [svn r2931] Auto-update of version strings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.8.1'
 
 f = open(os.path.join(os.path.dirname(__file__), 'docs', 'index.txt'))
 long_description = f.read().strip()
