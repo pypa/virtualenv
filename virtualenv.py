993ba1316a83b760370f5a3872b3f5ef4dd904c1
Ian Bicking <ianb@colorstudy.com>
2007-09-18 21:44:41 -0500

    [svn r2959] Use Scripts/ on Windows, as that seems to be the convention

--- virtualenv.py
+++ virtualenv.py
@@ -209,11 +209,14 @@ def create_environment(home_dir, site_packages=True, clear=False):
     first be cleared.
     """
     if sys.platform == 'win32':
-        lib_dir = join(home_dir, 'lib')
+        lib_dir = join(home_dir, 'Lib')
     else:
         lib_dir = join(home_dir, 'lib', py_version)
     inc_dir = join(home_dir, 'include', py_version)
-    bin_dir = join(home_dir, 'bin')
+    if sys.platform == 'win32':
+        bin_dir = join(home_dir, 'Scripts')
+    else:
+        bin_dir = join(home_dir, 'bin')
 
     if sys.executable.startswith(bin_dir):
         print 'Please use the *system* python to run this script'
