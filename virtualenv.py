b0a0dc0a3186d0e2536a9ead1c0e8051ad28f348
Ian Bicking <ianb@colorstudy.com>
2007-09-18 20:46:23 -0500

    [svn r2954] pick up from proper Windows stdlib dir

--- virtualenv.py
+++ virtualenv.py
@@ -226,7 +226,10 @@ def create_environment(home_dir, site_packages=True, clear=False):
 
     prefix = sys.prefix
     mkdir(lib_dir)
-    stdlib_dir = join(prefix, 'lib', py_version)
+    if sys.platform == 'win32':
+        stdlib_dir = join(prefix, 'lib', py_version)
+    else:
+        stdlib_dir = join(prefix, 'lib')
     for fn in os.listdir(stdlib_dir):
         if fn != 'site-packages' and os.path.splitext(fn)[0] in REQUIRED_MODULES:
             copyfile(join(stdlib_dir, fn), join(lib_dir, fn))
