0817c09b90ccf34725508ebb8765dd00102627fc
Ian Bicking <ianb@colorstudy.com>
2007-10-10 09:27:36 -0500

    [svn r3050] Auto-update of version strings

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     print 'Note: without Setuptools installed you will have to use "python -m virtualenv ENV"'
 import sys, os
 
-version = '0.8.5'
+version = '0.9.1'
 
 f = open(os.path.join(os.path.dirname(__file__), 'docs', 'index.txt'))
 long_description = f.read().strip()
