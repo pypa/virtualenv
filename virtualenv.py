27fa7e3a7860121ea5845bfb5b8db928ae343c67
Ian Bicking <ianb@colorstudy.com>
2009-11-06 17:22:48 -0600

    update version for release

--- virtualenv.py
+++ virtualenv.py
@@ -375,7 +375,7 @@ def filter_ez_setup(line, project_name='setuptools'):
 
 def main():
     parser = optparse.OptionParser(
-        version="1.4rc1",
+        version="1.4",
         usage="%prog [OPTIONS] DEST_DIR")
 
     parser.add_option(
