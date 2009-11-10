094d7a3c0584b6522958e2a0221cdac450fc9b04
Ian Bicking <ianb@colorstudy.com>
2009-11-10 10:28:15 -0500

    update pip to 0.6 and prepare for 0.4.1 release

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ copyright = '2009, Ian Bicking, The Open Planning Project'
 # The short X.Y version.
 version = '1.4'
 # The full version, including alpha/beta/rc tags.
-release = '1.4.post1'
+release = '1.4.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
