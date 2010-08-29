f0ed72244b16c24634bfe0f1ee8578ee284a6d49
Christopher Nilsson <christopher@otherchirps.net>
2010-08-30 00:52:09 +1000

    prompt tweaks.
    * The "Aspen magic" special case uses square brackets in the original POSIX script.
    * Removed extra space in the normal prompt, again to match the original script.

--- virtualenv_support/activate.fish
+++ virtualenv_support/activate.fish
@@ -61,9 +61,9 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
             if test $_checkbase = "__"
                 # special case for Aspen magic directories
                 # see http://www.zetadev.com/software/aspen/
-                printf "%s(%s)%s %s" (set_color -b blue white) (basename dirname "$VIRTUAL_ENV") (set_color normal) (_old_fish_prompt)
+                printf "%s[%s]%s %s" (set_color -b blue white) (basename dirname "$VIRTUAL_ENV") (set_color normal) (_old_fish_prompt)
             else
-                printf "%s(%s)%s %s" (set_color -b blue white) (basename "$VIRTUAL_ENV") (set_color normal) (_old_fish_prompt)
+                printf "%s(%s)%s%s" (set_color -b blue white) (basename "$VIRTUAL_ENV") (set_color normal) (_old_fish_prompt)
             end
         end 
         set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
