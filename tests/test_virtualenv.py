aaeecc4ff2285aa6a4a619eeaa504e6e3bf42192
Carl Meyer <carl@oddbird.net>
2011-04-21 10:06:55 -0500

    True is not a typical return value for abspath(); and mocking abspath() is not actually needed here.

--- tests/test_virtualenv.py
+++ tests/test_virtualenv.py
@@ -8,26 +8,21 @@ def test_version():
 
 
 @patch('os.path.exists')
-@patch('os.path.abspath')
-def test_resolve_interpreter_with_absolute_path(mock_abspath, mock_exists):
+def test_resolve_interpreter_with_absolute_path(mock_exists):
     """Should return absolute path if given and exists"""
-    mock_abspath.return_value = True
     mock_exists.return_value = True
     virtualenv.is_executable = Mock(return_value=True)
 
     exe = virtualenv.resolve_interpreter("/usr/bin/python42")
 
     assert exe == "/usr/bin/python42", "Absolute path should return as is"
-    mock_abspath.assert_called_with("/usr/bin/python42")
     mock_exists.assert_called_with("/usr/bin/python42")
     virtualenv.is_executable.assert_called_with("/usr/bin/python42")
 
 
 @patch('os.path.exists')
-@patch('os.path.abspath')
-def test_resolve_intepreter_with_nonexistant_interpreter(mock_abspath, mock_exists):
+def test_resolve_intepreter_with_nonexistant_interpreter(mock_exists):
     """Should exit when with absolute path if not exists"""
-    mock_abspath.return_value = True
     mock_exists.return_value = False
 
     try:
@@ -36,15 +31,12 @@ def test_resolve_intepreter_with_nonexistant_interpreter(mock_abspath, mock_exis
     except SystemExit:
         pass
 
-    mock_abspath.assert_called_with("/usr/bin/python42")
     mock_exists.assert_called_with("/usr/bin/python42")
 
 
 @patch('os.path.exists')
-@patch('os.path.abspath')
-def test_resolve_intepreter_with_invalid_interpreter(mock_abspath, mock_exists):
+def test_resolve_intepreter_with_invalid_interpreter(mock_exists):
     """Should exit when with absolute path if not exists"""
-    mock_abspath.return_value = True
     mock_exists.return_value = True
     virtualenv.is_executable = Mock(return_value=False)
 
@@ -54,6 +46,5 @@ def test_resolve_intepreter_with_invalid_interpreter(mock_abspath, mock_exists):
     except SystemExit:
         pass
 
-    mock_abspath.assert_called_with("/usr/bin/python42")
     mock_exists.assert_called_with("/usr/bin/python42")
     virtualenv.is_executable.assert_called_with("/usr/bin/python42")
