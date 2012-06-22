f4e7f2958cee6253a5333c71c2377e022a0e0e6c
Jannis Leidel <jannis@leidel.info>
2012-06-22 11:55:01 +0200

    Updated setup.py to not be a PEP8 violation.
    I know I know, I'm a hobgoblin.

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
-import sys, os, shutil
+import os
+import shutil
+import sys
+
 try:
     from setuptools import setup
-    entry_points = """
-[console_scripts]
-virtualenv = virtualenv:main
-virtualenv-%s.%s = virtualenv:main
-""" % sys.version_info[:2]
-    kw = {'entry_points': entry_points.strip(),
-          'zip_safe': False,
-          'test_suite': 'nose.collector',
-          'tests_require': ['nose', 'Mock'],
-          }
+    setup_params = {
+        'entry_points': {
+            'console_scripts': [
+                'virtualenv=virtualenv:main',
+                'virtualenv-%s.%s=virtualenv:main' % sys.version_info[:2]
+            ],
+        },
+        'zip_safe': False,
+        'test_suite': 'nose.collector',
+        'tests_require': ['nose', 'Mock'],
+    }
 except ImportError:
     from distutils.core import setup
     if sys.platform == 'win32':
         print('Note: without Setuptools installed you will have to use "python -m virtualenv ENV"')
-        kw = {}
+        setup_params = {}
     else:
         script = 'scripts/virtualenv'
         script_ver = script + '-%s.%s' % sys.version_info[:2]
         shutil.copy(script, script_ver)
-        kw = {'scripts': [script, script_ver]}
+        setup_params = {'scripts': [script, script_ver]}
 
 here = os.path.dirname(os.path.abspath(__file__))
 
@@ -43,13 +47,14 @@ try:
 except ImportError:
     pass
 
-setup(name='virtualenv',
-      # If you change the version here, change it in virtualenv.py and
-      # docs/conf.py as well
-      version="1.7.1.2.post1",
-      description="Virtual Python Environment builder",
-      long_description=long_description,
-      classifiers=[
+setup(
+    name='virtualenv',
+    # If you change the version here, change it in virtualenv.py and
+    # docs/conf.py as well
+    version="1.7.1.2.post1",
+    description="Virtual Python Environment builder",
+    long_description=long_description,
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -61,16 +66,15 @@ setup(name='virtualenv',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
-        ],
-      keywords='setuptools deployment installation distutils',
-      author='Ian Bicking',
-      author_email='ianb@colorstudy.com',
-      maintainer='Jannis Leidel, Carl Meyer and Brian Rosner',
-      maintainer_email='python-virtualenv@groups.google.com',
-      url='http://www.virtualenv.org',
-      license='MIT',
-      py_modules=['virtualenv'],
-      packages=['virtualenv_support'],
-      package_data={'virtualenv_support': ['*-py%s.egg' % sys.version[:3], '*.tar.gz']},
-      **kw
-      )
+    ],
+    keywords='setuptools deployment installation distutils',
+    author='Ian Bicking',
+    author_email='ianb@colorstudy.com',
+    maintainer='Jannis Leidel, Carl Meyer and Brian Rosner',
+    maintainer_email='python-virtualenv@groups.google.com',
+    url='http://www.virtualenv.org',
+    license='MIT',
+    py_modules=['virtualenv'],
+    packages=['virtualenv_support'],
+    package_data={'virtualenv_support': ['*-py%s.egg' % sys.version[:3], '*.tar.gz']},
+    **setup_params)
