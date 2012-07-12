1fa4d75f4a6300754fb4a8e33d409f6a4e9f04c4
Eduard-Cristian Stefan <alexandrul.ct@gmail.com>
2012-07-12 09:27:51 +0300

    remove special case for user_dir on win32 platform

--- virtualenv.py
+++ virtualenv.py
@@ -56,7 +56,6 @@ abiflags = getattr(sys, 'abiflags', '')
 
 user_dir = os.path.expanduser('~')
 if sys.platform == 'win32':
-    user_dir = os.environ.get('APPDATA', user_dir)  # Use %APPDATA% for roaming
     default_storage_dir = os.path.join(user_dir, 'virtualenv')
 else:
     default_storage_dir = os.path.join(user_dir, '.virtualenv')
