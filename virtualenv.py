pypa/virtualenv

commit 81990bc5d50cec8ea0179376087fb8bc67530f23

fix distutils-init to not rely on __init__.py
On Mac OS X Lion, Apple have only distributed the compiled
.pyc and .pyo files.

distutils-init used to directly 'exec' the real distils'
__init__.py file. Instead, we import the real module using
imp and grab the variables we're interested in.

virtualenv.py
@@ -2254,27 +2254,29 @@ QastYw==
 
 ##file distutils-init.py
 DISTUTILS_INIT = convert("""
-eJytV92L4zYQf9dfMU0ottuse7RvC6FQrg8Lxz2Ugz4si9HacqKuIxlJ2ST313fG8odkO9d7qGFB
-keZLv/nNjFaeWm0caMukX9nbuLxwo6Q6WBg2dFvqSsB2WEgLSjvg8C6NO/NGqHc46erciB1YDRcB
-JVdwtijpwGmoparAHQVYVzXylUH0beGpc8OV+wG+oLo2b6AVtLf2tkM19HY6WwevAj16N7jLHRhh
-ZSUsSEXGF1bR00N7c0etfkEtWUtRPVzzW/4VKmlE6bS5MYZLxU8C9ghF3nJ3zPsdOrLu7GRjC9oP
-JP7RUqUz8dRDkxdFLRtRFNkOktFAkjFZj+pKmxMt0thBBvtFEOlCZ7SfPXYXHnKV0yIdMdh8QbSD
-7IyuoOXlGz8glA5+tMDbVnBjKUkIrwcSLAHS6JI7iWng1m/erBOnydDvm4yJxgofRtFdoShyqaww
-Lv2wg9ntOjFxFSUiJSb4OixjUUSuKKSSDs21tyTLciN4lWYZY7XRQQjQ05M2dhRgqVUtD4w5c/Nh
-vXIrrDMIEBPXUrQOPuPd/jRGm7kA5hcXjG23CJErj/B6lk1ViKuDKT6otLAqcfCm9AWO+IfAHYTr
-EEK+2YBa3tCRmHxB/uuLhQeqhyM3FVHFw0oimAajz32JdM5FRdfJWyNqecV7I3u63w13NTKBmJKg
-0d9+Tfw1YlzyUp9OXFX5dIUeqWkDs6pxOW50ZsqGWzsJpZFETzj6KlFTTfNGfhWFbokmFlIrmjoQ
-oo/ixt0coTHc3ApEx1L3+KyVeJwVLKyI7uH5JRITDZqUFlmG7aIU6UJlF6Q0+z4Xi73cto10A0Wt
-aLPIDvumwaACIo5T/pDHTeGTuoPNJ+TLJottR4jnc4w9xMtg1tO/mvTxR9F3Ul8Gs92AOfvpsCuO
-yQ2terraR8YodGr0ha/Crk8RvCSWf5SUktczXSNfSLGeUPFuSKi6q489rDoJYPFdqpgKdta1F50m
-QK0+JN7INmjW4ooCNp3b7cPajrEFaV/Ijgnbwt80t7BnlLxpfLkL84CD0oC/ENCFWF86GIOfTVjt
-rbbymkyEJp3u9v302mCrjO6y8Q137NCrSqs6c6kFjQcGR4JZEHUnLi0dpbHQFAv2MeyGOFFw6DQY
-RNrBuOwfJJMLVdmLxPGXzCJOVmrc58OIk34XKalnLD6ioaeqdCV8I9zZKC/GvoO4iM6SzHGVjGNp
-Vio4NQr/OsFZV1IbGgTz+KgrjvFwppfSSChsK0p835R7ZJ9Pzp46bI8Ngug3l5233++ch90pBGMZ
-bOx0cJmxe0Hio6jSZVH09bt+yO7dP4RmprtAEhvxPSTp3XkXSTxcIkkjpsJhTad3kQ2FgJru/wj1
-Iqo4pv8AHiXuAx8crgM/Q3KmOwHfH79zP0ojU8HZCvTBafoTN4ehp3bY9PZWvPSyI/7fehbRh9HS
-XF7rY/E4brpxPOrF74wxKHrXli7rUp18evrj49NfSffPELaxUSjuSeP286DwgtEMcW1xKr/he4/6
-IQ6FUXP+0gkioHY5iwC9Eyx3HKO7af0zPPe+XyLnZDUk46jB7uZthV8rp+wuKxYMCxnzL9Vk9s4=
+eJytV1uL4zYUftevOE0ottuMW9q3gVDo7lKGLtuyDJQyDEZjK4k6jmQkJTPZX99zLF8k25nuQwNh
+FJ37d24aeWy0caAtk/5kL8PxhRsl1d5Cf4F/+qNuSl0JWPcHaUFpBxzO0rgTr4U6w1FXp1pswGp4
+EVByBSeLnA6chp1UFbiDAOuqWj4xiD5ruGvNcOW+gXsU1+YZtILm0lw2KIbWjifr4EmgRW8Gb7kD
+I6yshAWpSPlMK1q6aS7uoNUPKCV3UlQ3r/kl/wKVNKJ02lwYw6PiRwFbRCVvuDvk3Q2RrDs5WduC
+7gOOf7RU6YQ99dDkRbGTtSiKbAPJoCDJmNwN4kqbIx3S2EAG25kT6Uxm0J/dtgH3acvpkA4YrO4R
+7SA7gyloePnM9wilg28t8KYR3FhKEsLrgQRLgNS65E5iGrj1lxfrxHFU9MsqY6K2wrtRtCEURS6V
+FcalP25gEl3LZgSvi9GXLRVZXmteFT6v6aoYnR4ZVxv4pJWY6txAmiSIM35Jz5+//1a8v/v84d39
+H5//zrzBNbzTzaUNwIhanLHGMHJn5NPJCdu5bsRZWgy1KNCj2MU8pHbsZ2Gucw9ExnZGB3hB10t0
+sSE0S612cs+YMxeP4RO3wqJras/EaykaB58wER+M0WbKgJbxwNh6jfl05QGeTrKuCvHqYKwqqLSw
+KnHwrPQLHPCLWd4L16KBzWGDPvCKDtR2L9is+sXCDTXvgZuK6trXALFgzRh96vq5NS4qCidvjNjJ
+14xRqbe/a+52WLZU1gkq/fmnxIcR45KX+njkqsrHEDqkxgssQY3H4aJVU9bc2pEpjTi67qBPJXY0
+gHgtv4hCN1TTFlIr6l3ARB/yG29zhMZwc8G0YmPg8KHSu51MF1hg3cLDY8QmalSJtaMszrZSpDOR
+TZDS7OtMzO5y29TS9YPCiiaL9LA3FQbtGg03yl9b2j6pG1h9xHpZZbHuCPF8irGHeO7McvoXkz78
+6MaDVzC9DSpnOxLb5hjN0KkrV3vLGLlOW6nwXdgOVYKX2PL30voJgWHkMy7WFVR8GxbUru2PLSwa
+CWDxIzWah1EWptMuCVDb7ZNuwgWbRbwig02neju31oNvQdpnvEPC1vAXLVmcGSWva9/uwtzgVjfg
+AwIKiHWtgz74RYrd3mgrX5OxoEmmjb5btau8uUSxrJjvFyveEFqUmXLNyriv4IgxC7xu2aUlUhoz
+jb7gHMNpiOsPN2SNTqQtjPP5QTy5UJV9kbirk4nHyUKP+3wYcdRnkZJ4xmISbWhVpQvuG+FORnk2
+9hWFi+jMiznukmEtTVoFt0bhn1KFVCWNoZ4xj0ltcwzEiVxKK6GwjSjxMVZusfp8crY0YTtsEER/
+OZ+83X1rPJxOIRhzZ2OjvcmMXXMSd3ily3a5z5X1RHYt/hCaiewMSRzE15CkR/JVJJE4R5JWTIXL
+mqhXkQ2ZgIbu/wj1zKvYp/8AHjmuAx8Ql4GfIDmRHYHvyGfuV2mkKqAtQB9Q0++42fcztcWm07dg
+peMd8H/rWUQf9Jb28tIci9dx3a7jQS5+ZwxO0YO5dFmb6uTj3a/4Ok7a/9xwjA1M8Uwarh96gUf0
+pvdrjVv5Gd97NA9xKQyS05dO4AGNy4kHaJ1guWK4f1W35+/hobP9GBknrWExDhLsat4W6muByq5W
+xazCwor5Fy6WNxU=
 """)
 
 ##file distutils.cfg

virtualenv_embedded/distutils-init.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import warnings 
+import imp
 import opcode # opcode is not a virtualenv module, so we can use it to find the stdlib
               # Important! To work on pypy, this must be a module that resides in the
               # lib-python/modified-x.y.z directory
@@ -13,7 +14,10 @@ if os.path.normpath(distutils_path) == os.path.dirname(os.path.normpath(__file__
         "The virtualenv distutils package at %s appears to be in the same location as the system distutils?")
 else:
     __path__.insert(0, distutils_path)
-    exec(open(os.path.join(distutils_path, '__init__.py')).read())
+    real_distutils = imp.load_module("_virtualenv_distutils", None, distutils_path, ('', '', imp.PKG_DIRECTORY))
+    # Copy the relevant attributes
+    __revision__ = real_distutils.__revision__
+    __version__ = real_distutils.__version__
 
 from distutils import dist, sysconfig
 

commit b17c75c9a2554bab6defe5969d191876f16db5da

Fix #310, Python 3 bug introduced in 81990bc. Thanks Abhishek Sarkar for the report.

virtualenv.py
@@ -2253,29 +2253,29 @@ QastYw==
 
 ##file distutils-init.py
 DISTUTILS_INIT = convert("""
-eJytV1uL4zYUftevOE0ottuMW9q3gVDo7lKGLtuyDJQyDEZjK4k6jmQkJTPZX99zLF8k25nuQwNh
-FJ37d24aeWy0caAtk/5kL8PxhRsl1d5Cf4F/+qNuSl0JWPcHaUFpBxzO0rgTr4U6w1FXp1pswGp4
-EVByBSeLnA6chp1UFbiDAOuqWj4xiD5ruGvNcOW+gXsU1+YZtILm0lw2KIbWjifr4EmgRW8Gb7kD
-I6yshAWpSPlMK1q6aS7uoNUPKCV3UlQ3r/kl/wKVNKJ02lwYw6PiRwFbRCVvuDvk3Q2RrDs5WduC
-7gOOf7RU6YQ99dDkRbGTtSiKbAPJoCDJmNwN4kqbIx3S2EAG25kT6Uxm0J/dtgH3acvpkA4YrO4R
-7SA7gyloePnM9wilg28t8KYR3FhKEsLrgQRLgNS65E5iGrj1lxfrxHFU9MsqY6K2wrtRtCEURS6V
-FcalP25gEl3LZgSvi9GXLRVZXmteFT6v6aoYnR4ZVxv4pJWY6txAmiSIM35Jz5+//1a8v/v84d39
-H5//zrzBNbzTzaUNwIhanLHGMHJn5NPJCdu5bsRZWgy1KNCj2MU8pHbsZ2Gucw9ExnZGB3hB10t0
-sSE0S612cs+YMxeP4RO3wqJras/EaykaB58wER+M0WbKgJbxwNh6jfl05QGeTrKuCvHqYKwqqLSw
-KnHwrPQLHPCLWd4L16KBzWGDPvCKDtR2L9is+sXCDTXvgZuK6trXALFgzRh96vq5NS4qCidvjNjJ
-14xRqbe/a+52WLZU1gkq/fmnxIcR45KX+njkqsrHEDqkxgssQY3H4aJVU9bc2pEpjTi67qBPJXY0
-gHgtv4hCN1TTFlIr6l3ARB/yG29zhMZwc8G0YmPg8KHSu51MF1hg3cLDY8QmalSJtaMszrZSpDOR
-TZDS7OtMzO5y29TS9YPCiiaL9LA3FQbtGg03yl9b2j6pG1h9xHpZZbHuCPF8irGHeO7McvoXkz78
-6MaDVzC9DSpnOxLb5hjN0KkrV3vLGLlOW6nwXdgOVYKX2PL30voJgWHkMy7WFVR8GxbUru2PLSwa
-CWDxIzWah1EWptMuCVDb7ZNuwgWbRbwig02neju31oNvQdpnvEPC1vAXLVmcGSWva9/uwtzgVjfg
-AwIKiHWtgz74RYrd3mgrX5OxoEmmjb5btau8uUSxrJjvFyveEFqUmXLNyriv4IgxC7xu2aUlUhoz
-jb7gHMNpiOsPN2SNTqQtjPP5QTy5UJV9kbirk4nHyUKP+3wYcdRnkZJ4xmISbWhVpQvuG+FORnk2
-9hWFi+jMiznukmEtTVoFt0bhn1KFVCWNoZ4xj0ltcwzEiVxKK6GwjSjxMVZusfp8crY0YTtsEER/
-OZ+83X1rPJxOIRhzZ2OjvcmMXXMSd3ily3a5z5X1RHYt/hCaiewMSRzE15CkR/JVJJE4R5JWTIXL
-mqhXkQ2ZgIbu/wj1zKvYp/8AHjmuAx8Ql4GfIDmRHYHvyGfuV2mkKqAtQB9Q0++42fcztcWm07dg
-peMd8H/rWUQf9Jb28tIci9dx3a7jQS5+ZwxO0YO5dFmb6uTj3a/4Ok7a/9xwjA1M8Uwarh96gUf0
-pvdrjVv5Gd97NA9xKQyS05dO4AGNy4kHaJ1guWK4f1W35+/hobP9GBknrWExDhLsat4W6muByq5W
-xazCwor5Fy6WNxU=
+eJytV1uL4zYUfvevOE0ottuMW9q3gVDa3aUMXXbLMlDKMBiNrSTqOJKRlMxkf33PkXyRbGe7Dw2E
+UXTu37lpxLFV2oIyifAncxmOL0xLIfcG+gv80x9VW6maw7o/CANSWWBwFtqeWMPlGY6qPjV8A0bB
+C4eKSTgZ5LRgFeyErMEeOBhbN+Ipgeizhjtnhkn7DdyjuNLPoCS0l/ayQTG0djwZC08cLXozeMss
+aG5EzQ0IScpnWtHSTXuxByV/QCmxE7y+eS0uxWeoheaVVfqSJHiU7Mhhi6gULbOHorshkrEnKxpT
+0n3A8Y8SMpuwZx6aoix3ouFlmW8gHRSkeSJ2g7hU+kiHLDaQw3bmRDaTGfTnty7gPm0FHbIBg9U9
+oh1kZzAFLaue2R6htPCtAda2nGlDSUJ4PZBgCJBGVcwKTAMz/vJiLD+Oin5Z5QlvDPdulC6EsiyE
+NFzb7McNTKJzbJqzphx92VKRFY1idenzmq3K0emRcbWBD0ryqc4NZGmKOOOX9Pz5x+/l27tP797c
+f/z0d+4NruGNai8uAM0bfsYaw8itFk8ny41jsfpyO+BWlpqfhcG4yxLdi/0tQqoT4a8Vby382mt8
+p7XSo7aWGdPBc+b6utaBmCQ7rQKQoWtAuthQCiold2KfJIPTT8xwg9blPumc+YDZC/wYGdAyHpJk
+vUbHbHWAp5No6pK/WhhLEWrFjUwtPEv1Agf8YmnsuXUQYkeZoHm8ogP16gt2uHoxcEMdf2C6pmbw
+hUMsWGhanboh4IzzmsIpWs134jVPqD/c74bZHdY69UKKSn/+KfVhxLgUlToemayLMYQOqfEC61bh
+cbhwaqoGUzIyZRFHPmau5juaWqwRn3mpWmoEA5nhzS5gog/5jbcFQqOZvmBasZtwYlG93k5GEiyw
+buHhMWLjDarEGpMGB2LFs5nIJkhp/nUmZneFaRth++lieJtHepIvKgx6PJqIlD9X2j6pG1i9x3pZ
+5bHuCPFiirGHeO7McvoXkz786GaKVzC9DSpnOxJdc4xm6NSVq7lNEnKdVlnpu9BNYoKX2Iq3wvgh
+gGEUM66kK6j4NiyoneuPLSwaCWDxczgaolEWpiMyDVDb7dNuLAbriL8ig8mmeju31oNvQdpnvEPC
+1vAXbWacGRVrGt/uXN/gU0CDDwgooKRrHfTBb1/s9lYZ8ZqOBU0yLvpuP6+K9hLFsvIjeNhBi0KL
+MlOuWRn3FRwx5oHXjl0YImUx0+gLzjGchrgzca026ETmYJzPD+IpuKzNi8AFn048Thd63OdD86M6
+84zE8yQm0VqXdbbgvub2pKVnS76icBGdeTHHXTKspUmr4NYo/furFLKiMdQzFjHJNcdAnMhltBJK
+0/IKX3DVFqvPJ2dLE7bDBkH0l/PJ29074+F0CsGYOxsb7U3myTUncYfXqnLLfa6sJybX4g+hmcjO
+kMRBfA1JellfRRKJcyRpxdS4rIl6FdmQCWjo/o9Qz7yKffoP4JHjOvABcRn4CZIT2RH4jnxmfpVG
+qgLaAvQBNfuO6X0/Ux02nb4FKx3vgP+XnkX0QW9pLy/NsXgdN24dD3LxO2Nwil7Zlc1dqtP3d7/h
+kzp1/+7hGBuY4pk0XD/0Ao/oTe/XGrfyM773aB7iUhgkpy+dwAMalxMP0DrBcsVw/6p25+/hobP9
+GBknrWExDhLJ1bwt1NcCNblaFbMKCyvmX0PeRaQ=
 """)
 
 ##file distutils.cfg

virtualenv_embedded/distutils-init.py
@@ -16,7 +16,10 @@ else:
     __path__.insert(0, distutils_path)
     real_distutils = imp.load_module("_virtualenv_distutils", None, distutils_path, ('', '', imp.PKG_DIRECTORY))
     # Copy the relevant attributes
-    __revision__ = real_distutils.__revision__
+    try:
+        __revision__ = real_distutils.__revision__
+    except AttributeError:
+        pass
     __version__ = real_distutils.__version__
 
 from distutils import dist, sysconfig