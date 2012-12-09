f6e5e737cce519263e4e55af8b09977f7aa232c1
Preston Holmes <preston@ptone.com>
2012-12-08 21:20:24 -0800

    Use pypy readline even on OS X
    If the OS X special case is put in place for pypy
    then the lib_pypy directory is not created, and pypy
    will run without sys.prefix being set, which causes
    virtualenv to fail to build.

--- virtualenv.py
+++ virtualenv.py
@@ -1189,8 +1189,9 @@ def copy_required_modules(dst_prefix):
             else:
                 if f is not None:
                     f.close()
-                # special-case custom readline.so on OS X:
-                if modname == 'readline' and sys.platform == 'darwin' and not filename.endswith(join('lib-dynload', 'readline.so')):
+                # special-case custom readline.so on OS X, but not for pypy:
+                if modname == 'readline' and sys.platform == 'darwin' and not (
+                        is_pypy or filename.endswith(join('lib-dynload', 'readline.so'))):
                     dst_filename = join(dst_prefix, 'lib', 'python%s' % sys.version[:3], 'readline.so')
                 else:
                     dst_filename = change_prefix(filename, dst_prefix)
