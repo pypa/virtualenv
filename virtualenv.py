ab66436dbb62a36c21cc593e8303f14e1ffbb483
unknown <ramil@rbcmail.ru>
2013-07-13 02:28:30 +0600

    Bug fix: error at non ASCII symbols in the path

--- virtualenv.py
+++ virtualenv.py
@@ -1383,7 +1383,7 @@ def install_python(home_dir, lib_dir, inc_dir, bin_dir, site_packages, clear, sy
         py_executable = '"%s"' % py_executable
     # NOTE: keep this check as one line, cmd.exe doesn't cope with line breaks
     cmd = [py_executable, '-c', 'import sys;out=sys.stdout;'
-        'getattr(out, "buffer", out).write(sys.prefix.encode("utf-8"))']
+        'getattr(out, "buffer", out).write(sys.prefix.decode(sys.getfilesystemencoding()).encode("utf-8"))']
     logger.info('Testing executable with %s %s "%s"' % tuple(cmd))
     try:
         proc = subprocess.Popen(cmd,
@@ -1399,7 +1399,7 @@ def install_python(home_dir, lib_dir, inc_dir, bin_dir, site_packages, clear, sy
 
     proc_stdout = proc_stdout.strip().decode("utf-8")
     proc_stdout = os.path.normcase(os.path.abspath(proc_stdout))
-    norm_home_dir = os.path.normcase(os.path.abspath(home_dir))
+    norm_home_dir = os.path.normcase(os.path.abspath(home_dir).decode(sys.getfilesystemencoding())) 
     if hasattr(norm_home_dir, 'decode'):
         norm_home_dir = norm_home_dir.decode(sys.getfilesystemencoding())
     if proc_stdout != norm_home_dir:
