6db8fb1104a7ee910d19191cabd86c292ce09cb6
Jonathan Barronville <jonathan@belairlabs.com>
2014-04-09 03:24:57 -0400

    Brought back the namespacing ... my bad.

--- virtualenv_embedded/activate.fish
+++ virtualenv_embedded/activate.fish
@@ -2,17 +2,17 @@
 # Do not run it directly.
 
 function deactivate -d 'Exit virtualenv mode and return to the normal environment.'
-    if test -n $_OLD_PATH
-        set -gx PATH $_OLD_PATH
-        set -e _OLD_PATH
+    if test -n $_VIRTUALENV_OLD_PATH
+        set -gx PATH $_VIRTUALENV_OLD_PATH
+        set -e _VIRTUALENV_OLD_PATH
     end
 
-    if test -n $_OLD_PYTHONHOME
-        set -gx PYTHONHOME $_OLD_PYTHONHOME
-        set -e _OLD_PYTHONHOME
+    if test -n $_VIRTUALENV_OLD_PYTHONHOME
+        set -gx PYTHONHOME $_VIRTUALENV_OLD_PYTHONHOME
+        set -e _VIRTUALENV_OLD_PYTHONHOME
     end
 
-    if test -n $_OLD_FISH_PROMPT_OVERRIDE
+    if test -n $_VIRTUALENV_OLD_FISH_PROMPT_OVERRIDE
         # Set an empty local `$fish_function_path` to allow the removal of `fish_prompt` using `functions -e`.
         set -l fish_function_path
 
@@ -20,7 +20,7 @@ function deactivate -d 'Exit virtualenv mode and return to the normal environmen
         functions -e fish_prompt
         functions -c _old_fish_prompt fish_prompt
         functions -e _old_fish_prompt
-        set -e _OLD_FISH_PROMPT_OVERRIDE
+        set -e _VIRTUALENV_OLD_FISH_PROMPT_OVERRIDE
     end
 
     set -e VIRTUAL_ENV
@@ -36,12 +36,12 @@ deactivate nondestructive
 
 set -gx VIRTUAL_ENV "__VIRTUAL_ENV__"
 
-set -gx _OLD_PATH $PATH
+set -gx _VIRTUALENV_OLD_PATH $PATH
 set -gx PATH $VIRTUAL_ENV/"__BIN_NAME__" $PATH
 
 # Unset `$PYTHONHOME` if set.
 if set -q PYTHONHOME
-    set -gx _OLD_PYTHONHOME $PYTHONHOME
+    set -gx _VIRTUALENV_OLD_PYTHONHOME $PYTHONHOME
     set -e PYTHONHOME
 end
 
@@ -61,5 +61,5 @@ if test \( -z $VIRTUALENV_DISABLE_PROMPT \) -o \( -z $VIRTUAL_ENV_DISABLE_PROMPT
         _old_fish_prompt
     end
 
-    set -gx _OLD_FISH_PROMPT_OVERRIDE $VIRTUAL_ENV
+    set -gx _VIRTUALENV_OLD_FISH_PROMPT_OVERRIDE $VIRTUAL_ENV
 end
