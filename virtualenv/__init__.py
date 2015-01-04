38749a0033c2acc6c9fd8971749c68f93cb9c0db
Donald Stufft <donald@stufft.io>
2015-01-04 14:35:02 -0500

    Add virtualenv.create function to enable easy virtualenv creation

--- virtualenv/__init__.py
+++ virtualenv/__init__.py
@@ -4,9 +4,11 @@ from virtualenv.__about__ import (
     __author__, __copyright__, __email__, __license__, __summary__, __title__,
     __uri__, __version__
 )
+from virtualenv.core import create
 
 
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
     "__email__", "__license__", "__copyright__",
+    "create",
 ]
