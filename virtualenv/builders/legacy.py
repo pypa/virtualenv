f8a7bd83db1806fce241a6aca25d7498924988b2
Donald Stufft <donald@stufft.io>
2015-01-04 14:39:00 -0500

    Add from __future__ imports to all files

--- virtualenv/builders/legacy.py
+++ virtualenv/builders/legacy.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function
+
 import io
 import json
 import os.path
--- virtualenv/flavors/posix.py
+++ virtualenv/flavors/posix.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function
+
 import os.path
 
 from virtualenv.flavors.base import BaseFlavor
