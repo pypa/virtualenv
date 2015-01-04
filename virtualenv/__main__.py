464187ba56ebabe2a47899a51199bc4640d3363b
Donald Stufft <donald@stufft.io>
2015-01-04 16:26:19 -0500

    Move the CLI integration into it's own module

--- virtualenv/__main__.py
+++ virtualenv/__main__.py
@@ -2,10 +2,11 @@ from __future__ import absolute_import, division, print_function
 
 from functools import partial
 
-from virtualenv.core import cli
+from virtualenv.cli import cmd
 
 
-main = partial(cli, prog_name="virtualenv")
+main = partial(cmd, prog_name="virtualenv")
+
 
 if __name__ == "__main__":
     main()
