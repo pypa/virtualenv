811ae051e9bed3b5f1a5d1e760e3578b125e7cdb
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-01-06 00:20:43 +0200

    Add integration tests.

---
+++ tests/functional/test_create.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+import pytest
+import scripttest
+
+
+is_windows = (
+    sys.platform.startswith("win") or
+    (sys.platform == "cli" and os.name == "nt")
+)
+
+
+@pytest.yield_fixture
+def env(request):
+    env = scripttest.TestFileEnvironment()
+    try:
+        yield env
+    finally:
+        env.clear()
+
+
+def test_create_via_script(env):
+    result = env.run('virtualenv', 'myenv')
+    if is_windows:
+        assert 'myenv\\Scripts\\activate.bat' in result.files_created
+        assert 'myenv\\Scripts\\activate.ps1' in result.files_created
+        assert 'myenv\\Scripts\\activate_this.py' in result.files_created
+        assert 'myenv\\Scripts\\deactivate.bat' in result.files_created
+        assert 'myenv\\Scripts\\easy_install.exe' in result.files_created
+        assert 'myenv\\Scripts\\pip.exe' in result.files_created
+        assert 'myenv\\Scripts\\python.exe' in result.files_created
+        assert 'myenv\\Scripts\\python2.exe' in result.files_created
+    else:
+        assert 'myenv/bin/activate.sh' in result.files_created
+        assert 'myenv/bin/activate_this.py' in result.files_created
+        assert 'myenv/bin/python' in result.files_created
+
+def test_create_via_module(env):
+    result = env.run('python', '-mvirtualenv', 'myenv')
+    if is_windows:
+        assert 'myenv\\Scripts\\activate.bat' in result.files_created
+        assert 'myenv\\Scripts\\activate.ps1' in result.files_created
+        assert 'myenv\\Scripts\\activate_this.py' in result.files_created
+        assert 'myenv\\Scripts\\deactivate.bat' in result.files_created
+        assert 'myenv\\Scripts\\easy_install.exe' in result.files_created
+        assert 'myenv\\Scripts\\pip.exe' in result.files_created
+        assert 'myenv\\Scripts\\python.exe' in result.files_created
+        assert 'myenv\\Scripts\\python2.exe' in result.files_created
+    else:
+        assert 'myenv/bin/activate.sh' in result.files_created
+        assert 'myenv/bin/activate_this.py' in result.files_created
+        assert 'myenv/bin/python' in result.files_created
