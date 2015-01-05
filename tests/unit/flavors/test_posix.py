b4927a81a53f4196f0ae94d82fc03748721690bb
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-01-06 00:54:20 +0200

    Doooh, forgot the ".join" call

--- tests/unit/flavors/test_posix.py
+++ tests/unit/flavors/test_posix.py
@@ -28,11 +28,11 @@ def test_python_bins(version_info, expected):
 @pytest.mark.parametrize(
     ("version_info", "expected"),
     [
-        ([2, 6, 9, "final", 0], posixpath("lib", "python2.6")),
-        ([2, 7, 9, "final", 0], posixpath("lib", "python2.7")),
-        ([3, 2, 6, "final", 0], posixpath("lib", "python3.2")),
-        ([3, 3, 6, "final", 0], posixpath("lib", "python3.3")),
-        ([3, 4, 2, "final", 0], posixpath("lib", "python3.4")),
+        ([2, 6, 9, "final", 0], posixpath.join("lib", "python2.6")),
+        ([2, 7, 9, "final", 0], posixpath.join("lib", "python2.7")),
+        ([3, 2, 6, "final", 0], posixpath.join("lib", "python3.2")),
+        ([3, 3, 6, "final", 0], posixpath.join("lib", "python3.3")),
+        ([3, 4, 2, "final", 0], posixpath.join("lib", "python3.4")),
     ],
 )
 def test_lib_dir(version_info, expected):
