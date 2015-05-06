ecea25703692f36db733e85731c5bdb465ab23a8
Orion Poplawski <orion@cora.nwra.com>
2015-05-06 14:31:11 -0600

    Copy/symlink .pyc and .pyo files - fixes issue #659

--- virtualenv.py
+++ virtualenv.py
@@ -1118,11 +1118,15 @@ def copy_required_modules(dst_prefix, symlink):
                     pass
                 else:
                     dst_filename = change_prefix(filename, dst_prefix)
-                copyfile(filename, dst_filename, symlink)
                 if filename.endswith('.pyc'):
-                    pyfile = filename[:-1]
-                    if os.path.exists(pyfile):
-                        copyfile(pyfile, dst_filename[:-1], symlink)
+                    filename = filename[:-1]
+                    dst_filename = dst_filename[:-1]
+                copyfile(filename, dst_filename, symlink)
+                if filename.endswith('.py'):
+                    if os.path.exists(filename + 'c'):
+                        copyfile(filename + 'c', dst_filename + 'c', symlink)
+                    if os.path.exists(filename + 'o'):
+                        copyfile(filename + 'o', dst_filename + 'o', symlink)
     finally:
         sys.path = _prev_sys_path
 
