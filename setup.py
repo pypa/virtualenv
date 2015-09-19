fb41d271493785c3384537124fcc662e2573225f
Matthew Iversen <teh.ivo@gmail.com>
2015-09-19 21:49:18 +1000

    Add 3.3 and 3.4 flags in setup.py

--- setup.py
+++ setup.py
@@ -97,6 +97,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
     keywords='setuptools deployment installation distutils',
     author='Ian Bicking',
