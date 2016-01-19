0268876db0983d93ff5a7626ec8e42463a5d903c
Matt Iversen <teh.ivo@gmail.com>
2016-01-19 18:26:08 +1100

    Correct syntax and control logic flow

--- virtualenv_embedded/activate.bat
+++ virtualenv_embedded/activate.bat
@@ -12,14 +12,19 @@ if defined _OLD_VIRTUAL_PROMPT (
 set "PROMPT=__VIRTUAL_WINPROMPT__ %PROMPT%"
 
 REM Don't use () to avoid problems with them in %PATH%
-if not defined _OLD_VIRTUAL_PYTHONHOME
+if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
     set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+:ENDIFVHOME
 
 set PYTHONHOME=
 
-if defined _OLD_VIRTUAL_PATH
+REM if defined _OLD_VIRTUAL_PATH (
+if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
     set "PATH=%_OLD_VIRTUAL_PATH%"
-if not defined _OLD_VIRTUAL_PATH
+:ENDIFVPATH1
+REM ) else (
+if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
     set "_OLD_VIRTUAL_PATH=%PATH%"
+:ENDIFVPATH2
 
 set "PATH=%VIRTUAL_ENV%\__BIN_NAME__;%PATH%"
