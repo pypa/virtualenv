227a511d67ad66418b2f73ddce748e886d5322a4
Matthew Iversen <teh.ivo@gmail.com>
2016-01-19 17:45:59 +1100

    Remove some brackets in bat, solves #35

--- virtualenv_embedded/activate.bat
+++ virtualenv_embedded/activate.bat
@@ -7,20 +7,19 @@ if defined _OLD_VIRTUAL_PROMPT (
     if not defined PROMPT (
         set "PROMPT=$P$G"
     )
-	set "_OLD_VIRTUAL_PROMPT=%PROMPT%"	
+    set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
 )
 set "PROMPT=__VIRTUAL_WINPROMPT__ %PROMPT%"
 
-if not defined _OLD_VIRTUAL_PYTHONHOME (
+REM Don't use () to avoid problems with them in %PATH%
+if not defined _OLD_VIRTUAL_PYTHONHOME
     set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
-)
+
 set PYTHONHOME=
 
-if defined _OLD_VIRTUAL_PATH (
+if defined _OLD_VIRTUAL_PATH
     set "PATH=%_OLD_VIRTUAL_PATH%"
-) else (
+if not defined _OLD_VIRTUAL_PATH
     set "_OLD_VIRTUAL_PATH=%PATH%"
-)
-set "PATH=%VIRTUAL_ENV%\__BIN_NAME__;%PATH%"
 
-:END
+set "PATH=%VIRTUAL_ENV%\__BIN_NAME__;%PATH%"
