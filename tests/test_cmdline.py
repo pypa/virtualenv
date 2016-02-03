3c2e616d789240ed7e4a9783e554ccc3bad89047
Paul Moore <p.f.moore@gmail.com>
2016-02-03 14:47:31 +0000

    Abbreviated form on Windows is N.M, not pyNM

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -24,7 +24,7 @@ def test_commandline_explicit_interp(tmpdir):
 def test_commandline_abbrev_interp(tmpdir):
     """Specifying abbreviated forms of the Python interpreter should work"""
     if sys.platform == 'win32':
-        fmt = 'py%s%s'
+        fmt = '%s.%s'
     else:
         fmt = 'python%s.%s'
     abbrev = fmt % (sys.version_info[0], sys.version_info[1])
