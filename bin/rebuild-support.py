81f43fb9ec7c53ab9fd8f8d13a35274d6a02c977
Thomas Prideaux-Ghee <tprideauxghe@bloomberg.net>
2018-10-26 12:34:20 -0400

    Relative file path fix

--- bin/rebuild-support.py
+++ bin/rebuild-support.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 """
 Helper script to rebuild virtualenv_support. Downloads the wheel files using pip
 """
@@ -8,15 +6,22 @@ import glob
 import os
 import subprocess
 
-def removeWheelFiles():
-    for file in glob.glob("../src/virtualenv_support/*.whl"):
+
+def virtualenv_support_path():
+    return os.path.join(os.path.dirname(__file__), "../src/virtualenv_support")
+
+
+def remove_wheel_files():
+    for file in glob.glob(virtualenv_support_path() + "/*.whl"):
         os.remove(file)
 
-def download(package):
-    subprocess.call(["pip", "download", "-d", "../src/virtualenv_support", package])
 
-if __name__ == '__main__':
-    removeWheelFiles()
+def download(package):
+    subprocess.call(["pip", "download", "-d", virtualenv_support_path(), package])
+
+
+if __name__ == "__main__":
+    remove_wheel_files()
     download("pip")
     download("wheel")
     download("setuptools")
