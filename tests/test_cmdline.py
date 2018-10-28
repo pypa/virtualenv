57cd97a0111753ee1b1457aaf5fec158d0d798ef
Bernat Gabor <gaborjbernat@gmail.com>
2018-10-28 12:55:31 +0000

    Jython support

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 
@@ -5,12 +6,20 @@ import pytest
 
 import virtualenv
 
-VIRTUALENV_SCRIPT = virtualenv.__file__
+
+def get_src(path):
+    base, _ = os.path.splitext(path)
+    if virtualenv.is_jython and base.endswith("$py"):
+        base = base[:-3]  # strip away Jython ext
+    return "{}.py".format(base)
+
+
+VIRTUALENV_SCRIPT = get_src(virtualenv.__file__)
 
 
 def test_commandline_basic(tmpdir):
     """Simple command line usage should work"""
-    subprocess.check_call([sys.executable, VIRTUALENV_SCRIPT, str(tmpdir.join("venv"))])
+    subprocess.check_output([sys.executable, VIRTUALENV_SCRIPT, str(tmpdir.join("venv"))], stderr=subprocess.STDOUT)
 
 
 def test_commandline_explicit_interp(tmpdir):
@@ -26,9 +35,5 @@ def test_commandline_explicit_interp(tmpdir):
 @pytest.mark.skipif("sys.platform == 'win32' and sys.version_info[:1] >= (3,)")
 def test_commandline_abbrev_interp(tmpdir):
     """Specifying abbreviated forms of the Python interpreter should work"""
-    if sys.platform == "win32":
-        fmt = "%s.%s"
-    else:
-        fmt = "python%s.%s"
-    abbrev = fmt % (sys.version_info[0], sys.version_info[1])
+    abbrev = "{}{}.{}".format("" if sys.platform == "win32" else "python", *sys.version_info[0:2])
     subprocess.check_call([sys.executable, VIRTUALENV_SCRIPT, "-p", abbrev, str(tmpdir.join("venv"))])
