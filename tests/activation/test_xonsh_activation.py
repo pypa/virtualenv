a908591f4a584c09ed68c1da995b91952925acc4
Anthony Scopatz <scopatz@gmail.com>
2018-11-06 11:54:47 -0500

    put in some debug for tests

--- tests/activation/test_xonsh_activation.py
+++ tests/activation/test_xonsh_activation.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import os
 from os.path import join, normcase
 
 import pytest
@@ -7,7 +8,7 @@ import pytest
 import virtualenv
 from tests.lib import need_executable
 
-XONSH_COMMAND = "xonsh.bat" if virtualenv.is_win else "xonsh"
+XONSH_COMMAND = "xonsh.exe" if virtualenv.is_win else "xonsh"
 
 
 def need_xonsh(fn):
@@ -30,7 +31,12 @@ def test_activate_with_xonsh(tmpdir, monkeypatch):
         "-c",
         "{0}; source r'{1}'; {0}; deactivate; {0}".format(print_python_exe_path(), activate_script),
     ]
-    output = subprocess.check_output(cmd, universal_newlines=True, stderr=subprocess.STDOUT)
+    print('COMMAND', cmd)
+    print('Executable', sys.executable)
+    env = dict(os.environ)
+    env['XONSH_DEBUG'] = '1'
+    env['XONSH_SHOW_TRACEBACK'] = 'True'
+    output = subprocess.check_output(cmd, universal_newlines=True, stderr=subprocess.STDOUT, env=env)
     content = output.split()
     assert len(content) == 3, output
     before_activate, after_activate, after_deactivate = content
