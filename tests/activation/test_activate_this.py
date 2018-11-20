1cdee102d2e3571285fa863c7b2d719e45a648b9
nonylene <nonylene@gmail.com>
2018-11-20 06:27:07 +0000

    Fix syntax in test_activate_this.py

--- tests/activation/test_activate_this.py
+++ tests/activation/test_activate_this.py
@@ -6,19 +6,19 @@ def get_base_dir():
 
 
 def get_activate_this_path():
-    return os.path.abspath(os.path.join(get_base_dir(), 'virtualenv_embedded', 'activate_this.py'))
+    return os.path.abspath(os.path.join(get_base_dir(), "virtualenv_embedded", "activate_this.py"))
 
 
 def activate():
     activate_this = get_activate_this_path()
-    exec(compile(open(activate_this).read(), activate_this, 'exec'), dict(__file__=activate_this))
+    exec(compile(open(activate_this).read(), activate_this, "exec"), dict(__file__=activate_this))
 
 
 def test_activate_this():
     base_dir = get_base_dir()
-    old_path = os.environ['PATH']
+    old_path = os.environ["PATH"]
 
     activate()
 
-    assert os.environ['PATH'] == os.path.dirname(get_activate_this_path()) + os.pathsep + old_path
-    assert os.environ['VIRTUAL_ENV'] == base_dir
+    assert os.environ["PATH"] == os.path.dirname(get_activate_this_path()) + os.pathsep + old_path
+    assert os.environ["VIRTUAL_ENV"] == base_dir
