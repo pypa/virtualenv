04ce79630d6aa1df87a6bb44a550353c1961953e
Adam Sampson <ats-github@offog.org>
2018-12-18 07:34:06 +0000

    Dereference symlinks more thoroughly in copyfile (#956)
    copyfile was broken for symlinks that pointed to relative paths, and for
    symlinks that pointed to further symlinks (which meant that virtualenv
    failed to work in some cases when the Python executable it found was
    a symlink, e.g. when Python had been installed using stow or homebrew;
    issue #268).
    
    Make it follow symlinks until it finds an actual file to copy.

--- tests/test_virtualenv.py
+++ tests/test_virtualenv.py
@@ -337,6 +337,34 @@ def test_relative_symlink(tmpdir):
     assert os.path.exists(lib64)
 
 
+@pytest.mark.skipif(not hasattr(os, "symlink"), reason="requires working symlink implementation")
+def test_copyfile_from_symlink(tmp_path):
+    """Test that copyfile works correctly when the source is a symlink with a
+    relative target, and a symlink to a symlink. (This can occur when creating
+    an environment if Python was installed using stow or homebrew.)"""
+
+    # Set up src/link2 -> ../src/link1 -> file.
+    # We will copy to a different directory, so misinterpreting either symlink
+    # will be detected.
+    src_dir = tmp_path / "src"
+    src_dir.mkdir()
+    with open(str(src_dir / "file"), "w") as f:
+        f.write("contents")
+    os.symlink("file", str(src_dir / "link1"))
+    os.symlink(str(Path("..") / "src" / "link1"), str(src_dir / "link2"))
+
+    # Check that copyfile works on link2.
+    # This may produce a symlink or a regular file depending on the platform --
+    # which doesn't matter as long as it has the right contents.
+    copy_path = tmp_path / "copy"
+    virtualenv.copyfile(str(src_dir / "link2"), str(copy_path))
+    with open(str(copy_path), "r") as f:
+        assert f.read() == "contents"
+
+    shutil.rmtree(str(src_dir))
+    os.remove(str(copy_path))
+
+
 def test_missing_certifi_pem(tmp_path):
     """Make sure that we can still create virtual environment if pip is
     patched to not use certifi's cacert.pem and the file is removed.
