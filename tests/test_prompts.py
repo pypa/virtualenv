e7ea7a9026dc96f252583d46c8937aee617ac5c1
Brian Skinn <brian.skinn@gmail.com>
2019-02-17 22:27:15 -0500

    Cull duplicative tests & related helpers
    Also add docstrings.

--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -39,50 +39,19 @@ def clean_env():
     return os.environ.copy()
 
 
-def fix_PS1_return(result, trim_newline=True):
-    """Replace select byte values with escape sequences."""
-    result = result.replace(b"\x1b", b"\\e")
-    result = result.replace(b"\x07", b"\\a")
-
-    if trim_newline and result[-1:] == b"\n":
-        return result[:-1]
-    else:
-        return result
-
-
 @pytest.mark.parametrize(["command", "code"], [("dir", 0), ("exit 1", 1)])
 def test_exit_code(command, code, tmp_root):
+    """Confirm subprocess.call exit codes work as expected at the unit test level."""
     assert subprocess.call(command, cwd=str(tmp_root), shell=True) == code
 
 
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="Invalid on Windows")
 class TestBashPrompts:
-    @staticmethod
-    @pytest.fixture(scope="class")
-    def PS1():
-        return os.environ.get("PS1")
+    """Container for tests of bash prompt modifications."""
 
     @staticmethod
-    def test_suppressed_prompt_default_env_output(tmp_root, PS1, clean_env):
-        """Check correct form of PS1 via subprocess.check_output().
-
-        Duplicates test_suppressed_prompt_default_env_file, but demonstrates
-        how to go about handling a test like this without using a temporary
-        file, in case that's useful later.
-
-        """
-        clean_env.update({VIRTUAL_ENV_DISABLE_PROMPT: "1"})
-        command = '. {0}/bin/activate && echo "$PS1"'.format(ENV_DEFAULT)
-        result = subprocess.check_output(command, cwd=str(tmp_root), shell=True, env=clean_env)
-        result = fix_PS1_return(result)
-
-        # This assert MUST be made by encoding PS1, **NOT** by decoding
-        # result. Python's decoding machinery mangles the content of the
-        # returned $PS1 ~irretrievably.
-        assert result == PS1.encode()
-
-    @staticmethod
-    def test_suppressed_prompt_default_env_file(tmp_root, clean_env):
+    def test_suppressed_prompt_default_env(tmp_root, clean_env):
+        """Confirm VIRTUAL_ENV_DISABLE_PROMPT suppresses prompt changes on activate."""
         clean_env.update({VIRTUAL_ENV_DISABLE_PROMPT: "1"})
         command = 'echo "$PS1" > {1} && . {0}/bin/activate && echo "$PS1" >> {1}'.format(ENV_DEFAULT, OUTPUT_FILE)
 
@@ -94,6 +63,7 @@ class TestBashPrompts:
     @staticmethod
     @pytest.mark.parametrize(["env", "prefix"], [(ENV_DEFAULT, PREFIX_DEFAULT), (ENV_CUSTOM, PREFIX_CUSTOM)])
     def test_activated_prompt(env, prefix, tmp_root):
+        """Confirm prompt modification behavior with and without --prompt specified."""
         command = (
             'echo "$PS1" > {1} && . {0}/bin/activate && echo "$PS1" >> {1} ' '&& deactivate && echo "$PS1" >> {1}'
         ).format(env, OUTPUT_FILE)
