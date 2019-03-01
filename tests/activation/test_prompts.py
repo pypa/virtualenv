e0f7394c8a3699024ce2f9c11bf04a1c47490509
Brian Skinn <brian.skinn@gmail.com>
2019-02-28 21:28:07 -0500

    SQUASH (3): Various fixes and responses to review
    (1) Fix pathlib import for 2/3 compat
    
    (2) Refactor preamble_cmds to defaultdict
    
    (3) Convert shell info to namedtuples
    
    Also switch filename template string fields to autonumber.
    
    Also revert addition of black to setup.cfg

--- tests/activation/test_prompts.py
+++ tests/activation/test_prompts.py
@@ -4,31 +4,41 @@ from __future__ import absolute_import, unicode_literals
 import os
 import subprocess
 import sys
-from collections import defaultdict
-from pathlib import Path
+from collections import namedtuple
 from textwrap import dedent
 
 import pytest
 
 import virtualenv
 
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
+
+# This must match the DEST_DIR provided in the ../conftest.py:clean_python fixture
 ENV_DEFAULT = "env"
+
+# This can be anything
 ENV_CUSTOM = "env_custom"
 
+# Standard prefix, surround the env name in parentheses and separate by a space
 PREFIX_DEFAULT = "({}) ".format(ENV_DEFAULT)
+
+# Arbitrary prefix for the environment that's provided a 'prompt' arg
 PREFIX_CUSTOM = "---ENV---"
 
 VIRTUAL_ENV_DISABLE_PROMPT = "VIRTUAL_ENV_DISABLE_PROMPT"
 VIRTUAL_ENV = "VIRTUAL_ENV"
 
+# Filename template: {shell}.script.(normal|suppress).(default|custom)[extension]
+SCRIPT_TEMPLATE = "{}.script.{}.{}{}"
+
+# Filename template: {shell}.out.(normal|suppress).(default|custom)
+OUTPUT_TEMPLATE = "{}.out.{}.{}"
+
+
 SHELL_LIST = ["bash", "fish", "csh", "xonsh", "cmd", "powershell"]
-
-
-class EchoDict(dict):
-    """Returns the value for the key if available, else returns the key."""
-
-    def __getitem__(self, key):
-        return self.get(key, key)
 
 
 @pytest.fixture(scope="module")
@@ -54,7 +64,7 @@ def platform_check_skip(tmp_path_factory):
             test_ps1.write_text("echo foo\n")
 
             if 0 != subprocess.call("powershell -File {}".format(str(test_ps1)), shell=True):
-                pytest.skip("powershell script execution not enabled")
+                pytest.skip("powershell script execution fails; is it enabled?")
                 # Enable with:  PS> Set-ExecutionPolicy -scope currentuser -ExecutionPolicy Bypass -Force;
                 # Disable with: PS> Set-ExecutionPolicy -scope currentuser -ExecutionPolicy Restricted -Force;
 
@@ -99,53 +109,55 @@ def get_work_root(clean_python_root, custom_prompt_root):
 
 @pytest.fixture(scope="module")
 def shell_info():
-    class ShellInfo:
+    ShellInfo = namedtuple(
+        "ShellInfo",
+        [
+            "execute_cmd",
+            "prompt_cmd",
+            "activate_script",
+            "testscript_extension",
+            "preamble_cmd",
+            "activate_cmd",
+            "deactivate_cmd",
+        ],
+    )
 
-        execute_cmds = EchoDict({"cmd": "", "powershell": "powershell -File "})
+    # execute_cmd, prompt_cmd, activate_script are required.
+    # Defaults here are for testscript_extension, preamble_cmd, activate_cmd, and deactivate_cmd.
+    ShellInfo.__new__.__defaults__ = ("", "", "source ", "deactivate")
 
-        # {shell}.script.(normal|suppress).(default|custom)[extension]
-        script_template = "{0}.script.{1}.{2}{3}"
-
-        # {shell}.out.(normal|suppress).(default|custom)
-        output_template = "{0}.out.{1}.{2}"
-
-        testscript_extensions = defaultdict(lambda: "")
-        testscript_extensions.update({"powershell": ".ps1", "cmd": ".bat"})
-
-        preamble_cmds = {
-            "bash": "",
-            "fish": "",
-            "csh": "set prompt=%",
-            "xonsh": "$VIRTUAL_ENV = ''; $PROMPT = '{env_name}$ '",
-            "cmd": "@echo off & set PROMPT=$P$G",
-            "powershell": "",
-        }
-
-        prompt_cmds = {
-            "bash": 'echo "$PS1"',
-            "fish": "fish_prompt; echo ' '",
-            "csh": r"set | grep -E 'prompt\s' | sed -E 's/^prompt\s+(.*)$/\1/'",
-            "xonsh": "print(__xonsh__.shell.prompt)",
-            "cmd": "echo %PROMPT%",
-            "powershell": "prompt",
-        }
-
-        activate_cmds = defaultdict(lambda: "source ")
-        activate_cmds.update({"powershell": ". ", "cmd": "call "})
-
-        activate_scripts = {
-            "bash": "activate",
-            "fish": "activate.fish",
-            "csh": "activate.csh",
-            "xonsh": "activate.xsh",
-            "cmd": "activate.bat",
-            "powershell": "activate.ps1",
-        }
-
-        deactivate_cmds = defaultdict(lambda: "deactivate")
-        deactivate_cmds.update({"cmd": "call deactivate"})
-
-    return ShellInfo()
+    return {
+        "bash": ShellInfo(execute_cmd="bash", prompt_cmd='echo "$PS1"', activate_script="activate"),
+        "fish": ShellInfo(execute_cmd="fish", prompt_cmd="fish_prompt; echo ' '", activate_script="activate.fish"),
+        "csh": ShellInfo(
+            execute_cmd="csh",
+            prompt_cmd=r"set | grep -E 'prompt\s' | sed -E 's/^prompt\s+(.*)$/\1/'",
+            activate_script="activate.csh",
+            preamble_cmd="set prompt=%",
+        ),
+        "xonsh": ShellInfo(
+            execute_cmd="xonsh",
+            prompt_cmd="print(__xonsh__.shell.prompt)",
+            activate_script="activate.xsh",
+            preamble_cmd="$VIRTUAL_ENV = ''; $PROMPT = '{env_name}$ '",
+        ),
+        "cmd": ShellInfo(
+            execute_cmd="",
+            prompt_cmd="echo %PROMPT%",
+            activate_script="activate.bat",
+            preamble_cmd="@echo off & set PROMPT=$P$G",
+            testscript_extension=".bat",
+            activate_cmd="call ",
+            deactivate_cmd="call deactivate",
+        ),
+        "powershell": ShellInfo(
+            execute_cmd="powershell -File ",
+            prompt_cmd="prompt",
+            activate_script="activate.ps1",
+            testscript_extension=".ps1",
+            activate_cmd=". ",
+        ),
+    }
 
 
 @pytest.fixture(scope="function")
@@ -160,8 +172,8 @@ def test_suppressed_prompt(shell, env, get_work_root, clean_env, shell_info, pla
     """Confirm VIRTUAL_ENV_DISABLE_PROMPT suppresses prompt changes on activate."""
     platform_check_skip(sys.platform, shell)
 
-    script_name = shell_info.script_template.format(shell, "suppress", env, shell_info.testscript_extensions[shell])
-    output_name = shell_info.output_template.format(shell, "suppress", env)
+    script_name = SCRIPT_TEMPLATE.format(shell, "suppress", env, shell_info[shell].testscript_extension)
+    output_name = OUTPUT_TEMPLATE.format(shell, "suppress", env)
 
     clean_env.update({VIRTUAL_ENV_DISABLE_PROMPT: "1"})
 
@@ -180,16 +192,16 @@ def test_suppressed_prompt(shell, env, get_work_root, clean_env, shell_info, pla
         {prompt}
     """.format(
                 env=env,
-                act_cmd=shell_info.activate_cmds[shell],
-                preamble=shell_info.preamble_cmds[shell],
-                prompt=shell_info.prompt_cmds[shell],
-                act_script=shell_info.activate_scripts[shell],
+                act_cmd=shell_info[shell].activate_cmd,
+                preamble=shell_info[shell].preamble_cmd,
+                prompt=shell_info[shell].prompt_cmd,
+                act_script=shell_info[shell].activate_script,
                 bindir=work_root[1],
             )
         )
     )
 
-    command = "{} {} > {}".format(shell_info.execute_cmds[shell], script_name, output_name)
+    command = "{} {} > {}".format(shell_info[shell].execute_cmd, script_name, output_name)
 
     assert 0 == subprocess.call(command, cwd=str(work_root[0]), shell=True, env=clean_env)
 
@@ -205,8 +217,8 @@ def test_activated_prompt(shell, env, prefix, get_work_root, shell_info, platfor
     """Confirm prompt modification behavior with and without --prompt specified."""
     platform_check_skip(sys.platform, shell)
 
-    script_name = shell_info.script_template.format(shell, "normal", env, shell_info.testscript_extensions[shell])
-    output_name = shell_info.output_template.format(shell, "normal", env)
+    script_name = SCRIPT_TEMPLATE.format(shell, "normal", env, shell_info[shell].testscript_extension)
+    output_name = OUTPUT_TEMPLATE.format(shell, "normal", env)
 
     work_root = get_work_root(env)
 
@@ -225,17 +237,17 @@ def test_activated_prompt(shell, env, prefix, get_work_root, shell_info, platfor
         {prompt}
         """.format(
                 env=env,
-                act_cmd=shell_info.activate_cmds[shell],
-                deactivate=shell_info.deactivate_cmds[shell],
-                preamble=shell_info.preamble_cmds[shell],
-                prompt=shell_info.prompt_cmds[shell],
-                act_script=shell_info.activate_scripts[shell],
+                act_cmd=shell_info[shell].activate_cmd,
+                deactivate=shell_info[shell].deactivate_cmd,
+                preamble=shell_info[shell].preamble_cmd,
+                prompt=shell_info[shell].prompt_cmd,
+                act_script=shell_info[shell].activate_script,
                 bindir=work_root[1],
             )
         )
     )
 
-    command = "{} {} > {}".format(shell_info.execute_cmds[shell], script_name, output_name)
+    command = "{} {} > {}".format(shell_info[shell].execute_cmd, script_name, output_name)
 
     assert 0 == subprocess.call(command, cwd=str(work_root[0]), shell=True)
 
@@ -244,10 +256,11 @@ def test_activated_prompt(shell, env, prefix, get_work_root, shell_info, platfor
     # Before activation and after deactivation
     assert lines[1] == lines[3], lines
 
-    # Activated prompt. This construction copes with messes like fish's ANSI codes for colorizing.
-    # It's not as rigorous as I would like---it doesn't ensure no space is inserted between
+    # Activated prompt. This construction allows coping with messes like fish's ANSI codes for colorizing.
+    # The fish test is not as rigorous as I would like---it doesn't ensure no space is inserted between
     # a custom env prompt (argument to --prompt) and the base prompt---but it does provide assurance as
     # to the key pieces of content that should be present.
+    # The stricter test for the other shells is satisfactory, though.
     before, env_marker, after = lines[2].partition(prefix.encode("utf-8"))
     assert env_marker != b"", lines
 
