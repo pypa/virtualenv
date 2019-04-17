04a25e135e677de6fccb99ac1aa25a6963fa1b38
Paul Moore <p.f.moore@gmail.com>
2019-04-17 14:19:47 +0100

    Another attempt to get the Python 3.7.2 logic correct

--- virtualenv.py
+++ virtualenv.py
@@ -752,12 +752,19 @@ def main():
             # 3. Special case for Python 3.7.2, where we have a redirector,
             #    but sys._base_executable does not exist.
             if sys.version_info[:3] == (3, 7, 2):
-                # We are in a venv if base_prefix != prefix
-                if sys.base_prefix != sys.prefix:
-                    # We have to guess where the Python executable is. In a
-                    # normal installation, it's in sys.prefix, so we assume that.
-                    # If that file doesn't exist, we give up and don't reinvoke.
-                    base_exe = os.path.join(sys.base_prefix, "python.exe")
+                # We are in a venv if the environment variable __PYVENV_LAUNCHER__ is set.
+                if "__PYVENV_LAUNCHER__" in os.environ:
+                    # The base environment is either sys.real_prefix (if
+                    # we were invoked from a venv built from a virtualenv) or
+                    # sys.base_prefix if real_prefix doesn't exist (a simple venv).
+                    base_prefix = getattr(sys, "real_prefix", sys.base_prefix)
+                    # We assume the Python executable is directly under the prefix
+                    # directory. The only known case where that won't be the case is
+                    # an in-place source build, which we don't support. We don't need
+                    # to consider virtuale environments (where python.exe is in "Scripts"
+                    # because we've just followed the links back to a non-virtual
+                    # environment - we hope!)
+                    base_exe = os.path.join(base_prefix, "python.exe")
                     if os.path.exists(base_exe):
                         return base_exe
         # We don't need to reinvoke
@@ -772,8 +779,8 @@ def main():
         else:
             logger.notify("Running virtualenv with interpreter {}".format(interpreter))
             env["VIRTUALENV_INTERPRETER_RUNNING"] = "true"
-            # Remove the variable __PYVENV_LAUNCHER__ as it causes the interpreter
-            # to redirect back to the virtual environment.
+            # Remove the variable __PYVENV_LAUNCHER__ if it's present, as it causes the
+            # interpreter to redirect back to the virtual environment.
             if "__PYVENV_LAUNCHER__" in env:
                 del env["__PYVENV_LAUNCHER__"]
             file = __file__
