bb26a3c67ec8cf7d0e284c55fb01b0ce5673c097
John Vandenberg <jayvdb@gmail.com>
2019-06-25 23:13:44 +0700

    Lint activate.ps1
    Fix PSScriptAnalyzer warnings, and follow style from
    -Setting CodeFormatting
    
    Closes https://github.com/pypa/virtualenv/issues/1371

--- virtualenv_embedded/activate.ps1
+++ virtualenv_embedded/activate.ps1
@@ -1,38 +1,31 @@
 # This file must be dot sourced from PoSh; you cannot run it directly. Do this: . ./activate.ps1
 
 $script:THIS_PATH = $myinvocation.mycommand.path
-$script:BASE_DIR = split-path (resolve-path "$THIS_PATH/..") -Parent
+$script:BASE_DIR = Split-Path (Resolve-Path "$THIS_PATH/..") -Parent
 
-function global:deactivate([switch] $NonDestructive)
-{
-    if (test-path variable:_OLD_VIRTUAL_PATH)
-    {
+function global:deactivate([switch] $NonDestructive) {
+    if (Test-Path variable:_OLD_VIRTUAL_PATH) {
         $env:PATH = $variable:_OLD_VIRTUAL_PATH
-        remove-variable "_OLD_VIRTUAL_PATH" -scope global
+        Remove-Variable "_OLD_VIRTUAL_PATH" -Scope global
     }
 
-    if (test-path function:_old_virtual_prompt)
-    {
+    if (Test-Path function:_old_virtual_prompt) {
         $function:prompt = $function:_old_virtual_prompt
-        remove-item function:\_old_virtual_prompt
+        Remove-Item function:\_old_virtual_prompt
     }
 
-    if ($env:VIRTUAL_ENV)
-    {
-        $old_env = split-path $env:VIRTUAL_ENV -leaf
-        remove-item env:VIRTUAL_ENV -erroraction silentlycontinue
+    if ($env:VIRTUAL_ENV) {
+        Remove-Item env:VIRTUAL_ENV -ErrorAction SilentlyContinue
     }
 
-    if (!$NonDestructive)
-    {
+    if (!$NonDestructive) {
         # Self destruct!
-        remove-item function:deactivate
-        remove-item function:pydoc
+        Remove-Item function:deactivate
+        Remove-Item function:pydoc
     }
 }
 
-function global:pydoc
-{
+function global:pydoc {
     python -m pydoc $args
 }
 
@@ -42,31 +35,28 @@ deactivate -nondestructive
 $VIRTUAL_ENV = $BASE_DIR
 $env:VIRTUAL_ENV = $VIRTUAL_ENV
 
-$global:_OLD_VIRTUAL_PATH = $env:PATH
+New-Variable -Scope global -Name _OLD_VIRTUAL_PATH -Value $env:PATH
+
 $env:PATH = "$env:VIRTUAL_ENV/__BIN_NAME____PATH_SEP__" + $env:PATH
-if (!$env:VIRTUAL_ENV_DISABLE_PROMPT)
-{
-    function global:_old_virtual_prompt
-    {
+if (!$env:VIRTUAL_ENV_DISABLE_PROMPT) {
+    function global:_old_virtual_prompt {
         ""
     }
     $function:_old_virtual_prompt = $function:prompt
-    if ("__VIRTUAL_PROMPT__" -ne "")
-    {
-        function global:prompt
-        {
+
+    if ("__VIRTUAL_PROMPT__" -ne "") {
+        function global:prompt {
             # Add the custom prefix to the existing prompt
-            write-host "__VIRTUAL_PROMPT__" -nonewline
-            & $function:_old_virtual_prompt
+            $previous_prompt_value = & $function:_old_virtual_prompt
+            ("__VIRTUAL_PROMPT__" + $previous_prompt_value)
         }
     }
-    else
-    {
-        function global:prompt
-        {
+    else {
+        function global:prompt {
             # Add a prefix to the current prompt, but don't discard it.
-            write-host "($( split-path $env:VIRTUAL_ENV -leaf )) " -nonewline
-            & $function:_old_virtual_prompt
+            $previous_prompt_value = & $function:_old_virtual_prompt
+            $new_prompt_value = "($( Split-Path $env:VIRTUAL_ENV -Leaf )) "
+            ($new_prompt_value + $previous_prompt_value)
         }
     }
 }
