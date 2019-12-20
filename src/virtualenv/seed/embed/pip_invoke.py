ccea7b6c1b2b3e91ff8207f9e4a6fb56b6dd4cc0
Bernat Gabor <bgabor8@bloomberg.net>
2019-12-20 15:45:49 +0000

    add pip resolver
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- src/virtualenv/seed/embed/pip_invoke.py
+++ src/virtualenv/seed/embed/pip_invoke.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
-from .base_embed import BaseEmbed
+import os
+import subprocess
+
+from virtualenv.seed.embed.base_embed import BaseEmbed
+from virtualenv.seed.embed.wheels.acquire import get_bundled_wheel
 
 
 class PipInvoke(BaseEmbed):
@@ -10,3 +14,29 @@ class PipInvoke(BaseEmbed):
     def run(self, creator):
         if not self.enabled:
             return
+
+        version = creator.interpreter.version_release_str
+
+        cmd = [str(creator.exe), "-m", "pip", "install", "--only-binary", ":all:"]
+        for folder in {get_bundled_wheel(p, version).parent for p in ("pip", "setuptools")}:
+            cmd.extend(["--find-links", str(folder)])
+        if not self.download:
+            cmd.append("--no-index")
+        for key, version in {"pip": self.pip_version, "setuptools": self.setuptools_version}.items():
+            cmd.append("{}{}".format(key, "=={}".format(version) if version is not None else ""))
+
+        env = os.environ.copy()
+        env.update(
+            {
+                str(k): str(v)  # python 2 requires these to be string only (non-unicode)
+                for k, v in {
+                    # put the bundled wheel onto the path, and use it to do the bootstrap operation
+                    "PYTHONPATH": get_bundled_wheel("pip", version),
+                    "PIP_USE_WHEEL": "1",
+                    "PIP_USER": "0",
+                    "PIP_NO_INPUT": "1",
+                }.items()
+            }
+        )
+
+        subprocess.call(cmd, env=env)
