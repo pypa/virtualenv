0b8b30482e178dbb66e594bbe5fc228cc184bf77
Bernat Gabor <bgabor8@bloomberg.net>
2020-01-09 16:52:42 +0000

    xonosh is actually just xonsh
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- tests/unit/activation/test_xonosh.py
+++ tests/unit/activation/test_xonosh.py
@@ -4,26 +4,26 @@ import sys
 
 import pytest
 
-from virtualenv.activation import XonoshActivator
+from virtualenv.activation import XonshActivator
 from virtualenv.info import IS_PYPY, PY3
 
 
 @pytest.mark.skipif(sys.platform == "win32" and IS_PYPY and PY3, reason="xonsh on Windows blocks indefinitely")
-def test_xonosh(activation_tester_class, activation_tester):
-    class Xonosh(activation_tester_class):
+def test_xonsh(activation_tester_class, activation_tester):
+    class Xonsh(activation_tester_class):
         def __init__(self, session):
-            super(Xonosh, self).__init__(
-                XonoshActivator, session, "xonsh.exe" if sys.platform == "win32" else "xonsh", "activate.xsh", "xsh"
+            super(Xonsh, self).__init__(
+                XonshActivator, session, "xonsh.exe" if sys.platform == "win32" else "xonsh", "activate.xsh", "xsh"
             )
             self._invoke_script = [sys.executable, "-m", "xonsh"]
             self._version_cmd = [sys.executable, "-m", "xonsh", "--version"]
 
         def env(self, tmp_path):
-            env = super(Xonosh, self).env(tmp_path)
+            env = super(Xonsh, self).env(tmp_path)
             env.update({"XONSH_DEBUG": "1", "XONSH_SHOW_TRACEBACK": "True"})
             return env
 
         def activate_call(self, script):
             return "{} {}".format(self.activate_cmd, repr(str(script))).strip()
 
-    activation_tester(Xonosh)
+    activation_tester(Xonsh)
