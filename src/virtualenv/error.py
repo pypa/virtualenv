d57fffb231af2cd2b29266ad1303d73c1a437a1c
Bernat Gabor <bgabor8@bloomberg.net>
2020-01-10 15:29:49 +0000

    super for __init__ in exception_
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- src/virtualenv/error.py
+++ src/virtualenv/error.py
@@ -6,6 +6,7 @@ class ProcessCallFailed(RuntimeError):
     """Failed a process call"""
 
     def __init__(self, code, out, err, cmd):
+        super(ProcessCallFailed, self).__init__(code, out, err, cmd)
         self.code = code
         self.out = out
         self.err = err
