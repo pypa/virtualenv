5bd95d982589f76d13dcf0478e77ec34d5071fbe
Bernat Gabor <bgabor8@bloomberg.net>
2020-01-13 10:39:06 +0000

    bump bundled setuptools
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- src/virtualenv/seed/embed/wheels/__init__.py
+++ src/virtualenv/seed/embed/wheels/__init__.py
@@ -3,27 +3,27 @@ from __future__ import absolute_import, unicode_literals
 BUNDLE_SUPPORT = {
     "3.9": {
         "pip": "pip-19.3.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-44.0.0-py2.py3-none-any.whl",
+        "setuptools": "setuptools-45.0.0-py2.py3-none-any.whl",
         "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
     },
     "3.8": {
         "pip": "pip-19.3.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-44.0.0-py2.py3-none-any.whl",
+        "setuptools": "setuptools-45.0.0-py2.py3-none-any.whl",
         "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
     },
     "3.7": {
         "pip": "pip-19.3.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-44.0.0-py2.py3-none-any.whl",
+        "setuptools": "setuptools-45.0.0-py2.py3-none-any.whl",
         "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
     },
     "3.6": {
         "pip": "pip-19.3.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-44.0.0-py2.py3-none-any.whl",
+        "setuptools": "setuptools-45.0.0-py2.py3-none-any.whl",
         "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
     },
     "3.5": {
         "pip": "pip-19.3.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-44.0.0-py2.py3-none-any.whl",
+        "setuptools": "setuptools-45.0.0-py2.py3-none-any.whl",
         "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
     },
     "3.4": {
