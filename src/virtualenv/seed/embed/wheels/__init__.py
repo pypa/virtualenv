4d5a88e66a180df79cc676262afcb6c5ca6cfbde
Bernat Gabor <bgabor8@bloomberg.net>
2020-01-27 12:16:34 +0000

    bump pip and wheel
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- src/virtualenv/seed/embed/wheels/__init__.py
+++ src/virtualenv/seed/embed/wheels/__init__.py
@@ -2,29 +2,29 @@ from __future__ import absolute_import, unicode_literals
 
 BUNDLE_SUPPORT = {
     "3.9": {
-        "pip": "pip-20.0.1-py2.py3-none-any.whl",
+        "pip": "pip-20.0.2-py2.py3-none-any.whl",
         "setuptools": "setuptools-45.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
+        "wheel": "wheel-0.34.0-py2.py3-none-any.whl",
     },
     "3.8": {
-        "pip": "pip-20.0.1-py2.py3-none-any.whl",
+        "pip": "pip-20.0.2-py2.py3-none-any.whl",
         "setuptools": "setuptools-45.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
+        "wheel": "wheel-0.34.0-py2.py3-none-any.whl",
     },
     "3.7": {
-        "pip": "pip-20.0.1-py2.py3-none-any.whl",
+        "pip": "pip-20.0.2-py2.py3-none-any.whl",
         "setuptools": "setuptools-45.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
+        "wheel": "wheel-0.34.0-py2.py3-none-any.whl",
     },
     "3.6": {
-        "pip": "pip-20.0.1-py2.py3-none-any.whl",
+        "pip": "pip-20.0.2-py2.py3-none-any.whl",
         "setuptools": "setuptools-45.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
+        "wheel": "wheel-0.34.0-py2.py3-none-any.whl",
     },
     "3.5": {
-        "pip": "pip-20.0.1-py2.py3-none-any.whl",
+        "pip": "pip-20.0.2-py2.py3-none-any.whl",
         "setuptools": "setuptools-45.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
+        "wheel": "wheel-0.34.0-py2.py3-none-any.whl",
     },
     "3.4": {
         "pip": "pip-19.1.1-py2.py3-none-any.whl",
@@ -32,9 +32,9 @@ BUNDLE_SUPPORT = {
         "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
     },
     "2.7": {
-        "pip": "pip-20.0.1-py2.py3-none-any.whl",
+        "pip": "pip-20.0.2-py2.py3-none-any.whl",
         "setuptools": "setuptools-44.0.0-py2.py3-none-any.whl",
-        "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
+        "wheel": "wheel-0.34.0-py2.py3-none-any.whl",
     },
 }
 MAX = "3.9"
