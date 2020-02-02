8d1e4c065d9849e7e48bb9cc42761c261bf4ce1b
Bernat Gabor <bgabor8@bloomberg.net>
2020-02-02 11:55:43 +0000

    pyproject.toml for version generation
    Instead of the usual setup.py.
    
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-
-import textwrap
-
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        "write_to": "src/virtualenv/version.py",
-        "write_to_template": textwrap.dedent(
-            """
-             # coding: utf-8
-             from __future__ import unicode_literals
-             __version__ = {version!r}
-             """
-        ).lstrip(),
-    }
-)
+setup()
