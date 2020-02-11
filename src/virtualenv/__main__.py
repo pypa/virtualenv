6efd327d635d70388c240a6d40d4b26b77caacb6
Bernat Gabor <bgabor8@bloomberg.net>
2020-02-11 12:51:02 +0000

    add option to view failure stacktraces
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- src/virtualenv/__main__.py
+++ src/virtualenv/__main__.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
+import argparse
 import logging
 import sys
 from datetime import datetime
 
 
-def run(args=None):
+def run(args=None, options=None):
     start = datetime.now()
     from virtualenv.error import ProcessCallFailed
     from virtualenv.run import run_via_cli
@@ -13,7 +14,7 @@ def run(args=None):
     if args is None:
         args = sys.argv[1:]
     try:
-        run_via_cli(args)
+        run_via_cli(args, options)
     except ProcessCallFailed as exception:
         print("subprocess call failed for {}".format(exception.cmd))
         print(exception.out, file=sys.stdout, end="")
@@ -24,11 +25,16 @@ def run(args=None):
 
 
 def run_with_catch(args=None):
+    options = argparse.Namespace()
     try:
-        run(args)
+        run(args, options)
     except (KeyboardInterrupt, Exception) as exception:
-        logging.error("%s: %s", type(exception).__name__, exception)
-        sys.exit(1)
+        if options.with_traceback:
+            logging.shutdown()  # force flush of log messages before the trace is printed
+            raise
+        else:
+            logging.error("%s: %s", type(exception).__name__, exception)
+            sys.exit(1)
 
 
 if __name__ == "__main__":
