1d9b7d1898a31258a8a4af588b0cbe02081a3a0c
Bernát Gábor <bgabor8@bloomberg.net>
2020-02-14 16:08:01 +0000

    Better error message when build dependencies not satisfied (#1628)
    This only manifests on old platforms, but as we still support them
    makes sense to provide better error messages, and relax a bit our
    build requirements.
    
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
-from setuptools import setup
+from setuptools import __version__, setup
 
-setup()
+if int(__version__.split(".")[0]) < 41:
+    raise RuntimeError("setuptools >= 41 required to build")
+
+setup(
+    use_scm_version={"write_to": "src/virtualenv/version.py", "write_to_template": '__version__ = "{version}"'},
+    setup_requires=["setuptools_scm >= 2"],
+)
