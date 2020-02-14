7aae8f9d1a8a186761c2b18841748838d638202c
Bernat Gabor <bgabor8@bloomberg.net>
2020-02-14 16:29:18 +0000

    raise build error if we have setuptools less than 42
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import __version__, setup
 
-if int(__version__.split(".")[0]) < 41:
-    raise RuntimeError("setuptools >= 41 required to build")
+if int(__version__.split(".")[0]) < 42:
+    raise RuntimeError("setuptools >= 42 required to build")
 
 setup(
     use_scm_version={"write_to": "src/virtualenv/version.py", "write_to_template": '__version__ = "{version}"'},
