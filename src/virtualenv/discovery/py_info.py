9f980d8d95a6cd80f05c6144c01e0ffbad7d477d
Bernat Gabor <bgabor8@bloomberg.net>
2020-02-25 10:34:36 +0000

    Accept python self-referencing python executables
    Accept these as if they were system executables, print a info about
    them referencing themselves.
    
    Resolves #1632.
    
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- src/virtualenv/discovery/py_info.py
+++ src/virtualenv/discovery/py_info.py
@@ -314,13 +314,17 @@ class PythonInfo(object):
         while target.system_executable is None:
             prefix = target.real_prefix or target.base_prefix or target.prefix
             if prefix in prefixes:
+                if len(prefixes) == 1:
+                    # if we're linking back to ourselves accept ourselves with a WARNING
+                    logging.info("%r links back to itself via prefixes", target)
+                    target.system_executable = target.executable
+                    break
                 for at, (p, t) in enumerate(prefixes.items(), start=1):
                     logging.error("%d: prefix=%s, info=%r", at, p, t)
                 logging.error("%d: prefix=%s, info=%r", len(prefixes) + 1, prefix, target)
                 raise RuntimeError("prefixes are causing a circle {}".format("|".join(prefixes.keys())))
             prefixes[prefix] = target
             target = target.discover_exe(prefix=prefix, exact=False)
-
         if target.executable != target.system_executable:
             target = cls.from_exe(target.system_executable)
         target.executable = start_executable
