199efd0c16c98114f5cfa5bd2e3b8a4a696ffbc5
spetafree <6402127+spetafree@users.noreply.github.com>
2020-02-28 12:22:05 +0200

    Handle batch prompt in activate.bat

--- src/virtualenv/activation/batch/__init__.py
+++ src/virtualenv/activation/batch/__init__.py
@@ -21,9 +21,3 @@ class BatchActivator(ViaTemplateActivator):
         # ensure the text has all newlines as \r\n - required by batch
         base = super(BatchActivator, self).instantiate_template(replacements, template, creator)
         return base.replace(os.linesep, "\n").replace("\n", os.linesep)
-
-    def replacements(self, creator, dest_folder):
-        base_replacements = super(BatchActivator, self).replacements(creator, dest_folder)
-        if not base_replacements["__VIRTUAL_PROMPT__"]:
-            base_replacements["__VIRTUAL_PROMPT__"] = "(%s) " % creator.env_name
-        return base_replacements
