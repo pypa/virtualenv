a9bcaf22b026dddee2431bde28eb1a1257fe68de
Bernat Gabor <bgabor8@bloomberg.net>
2020-05-03 10:25:51 +0100

    add test
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

---
+++ tests/unit/activation/test_activate_this.py
@@ -0,0 +1,24 @@
+from virtualenv.activation import PythonActivator
+from virtualenv.config.cli.parser import VirtualEnvOptions
+from virtualenv.run import session_via_cli
+
+
+def test_from_py3_to_py2(session_app_data, cross_python, special_name_dir):
+    options = VirtualEnvOptions()
+    cli_args = [
+        str(special_name_dir),
+        "-p",
+        str(cross_python.executable),
+        "--app-data",
+        str(session_app_data.path),
+        "--without-pip",
+        "--activators",
+        "",
+    ]
+    session = session_via_cli(cli_args, options)
+    activator = PythonActivator(options)
+    session.creator.bin_dir.mkdir(parents=True)
+    result = activator.generate(session.creator)
+    content = result.read_text()
+    # check that the repr strings have been correctly stripped
+    assert "\"'" not in content
