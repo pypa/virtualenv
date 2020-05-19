078ed06eb7e46e3ef4286528da4cf40023c53e56
Bernát Gábor <bgabor8@bloomberg.net>
2020-05-19 12:38:29 +0100

    Upgrade pip and setuptools (#1836)

--- src/virtualenv/seed/embed/wheels/__init__.py
+++ src/virtualenv/seed/embed/wheels/__init__.py
@@ -2,28 +2,28 @@ from __future__ import absolute_import, unicode_literals
 
 BUNDLE_SUPPORT = {
     "3.9": {
-        "pip": "pip-20.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-46.3.1-py3-none-any.whl",
+        "pip": "pip-20.1.1-py2.py3-none-any.whl",
+        "setuptools": "setuptools-46.4.0-py3-none-any.whl",
         "wheel": "wheel-0.34.2-py2.py3-none-any.whl",
     },
     "3.8": {
-        "pip": "pip-20.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-46.3.1-py3-none-any.whl",
+        "pip": "pip-20.1.1-py2.py3-none-any.whl",
+        "setuptools": "setuptools-46.4.0-py3-none-any.whl",
         "wheel": "wheel-0.34.2-py2.py3-none-any.whl",
     },
     "3.7": {
-        "pip": "pip-20.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-46.3.1-py3-none-any.whl",
+        "pip": "pip-20.1.1-py2.py3-none-any.whl",
+        "setuptools": "setuptools-46.4.0-py3-none-any.whl",
         "wheel": "wheel-0.34.2-py2.py3-none-any.whl",
     },
     "3.6": {
-        "pip": "pip-20.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-46.3.1-py3-none-any.whl",
+        "pip": "pip-20.1.1-py2.py3-none-any.whl",
+        "setuptools": "setuptools-46.4.0-py3-none-any.whl",
         "wheel": "wheel-0.34.2-py2.py3-none-any.whl",
     },
     "3.5": {
-        "pip": "pip-20.1-py2.py3-none-any.whl",
-        "setuptools": "setuptools-46.3.1-py3-none-any.whl",
+        "pip": "pip-20.1.1-py2.py3-none-any.whl",
+        "setuptools": "setuptools-46.4.0-py3-none-any.whl",
         "wheel": "wheel-0.34.2-py2.py3-none-any.whl",
     },
     "3.4": {
@@ -32,7 +32,7 @@ BUNDLE_SUPPORT = {
         "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
     },
     "2.7": {
-        "pip": "pip-20.1-py2.py3-none-any.whl",
+        "pip": "pip-20.1.1-py2.py3-none-any.whl",
         "setuptools": "setuptools-44.1.0-py2.py3-none-any.whl",
         "wheel": "wheel-0.34.2-py2.py3-none-any.whl",
     },
