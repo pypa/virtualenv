7898c09c9ca218ea210df975ac0171129abdcbc9
Anthony Sottile <asottile@umich.edu>
2020-07-21 10:06:08 -0700

    add skip for bash test on windows

--- tests/unit/activation/test_bash.py
+++ tests/unit/activation/test_bash.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
+import sys
+
+import pytest
+
 from virtualenv.activation import BashActivator
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Github Actions ships with WSL bash")
 def test_bash(raise_on_non_source_class, activation_tester):
     class Bash(raise_on_non_source_class):
         def __init__(self, session):
