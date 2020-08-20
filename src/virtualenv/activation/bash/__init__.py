ee6f055100de5db7de36ae2441c605e6062a99d4
Bernat Gabor <bgabor8@bloomberg.net>
2020-08-20 22:20:46 +0100

    Use read_binary/write_bytes to avoid line normalization
    Signed-off-by: Bernat Gabor <bgabor8@bloomberg.net>

--- src/virtualenv/activation/bash/__init__.py
+++ src/virtualenv/activation/bash/__init__.py
@@ -1,35 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
-from virtualenv.info import IS_WIN
 from virtualenv.util.path import Path
-from virtualenv.util.six import ensure_text
 
 from ..via_template import ViaTemplateActivator
 
 
 class BashActivator(ViaTemplateActivator):
-    def generate(self, creator):
-        generated = super(BashActivator, self).generate(creator)
-        if IS_WIN:
-            _convert_to_unix_line_endings(generated)
-        return generated
-
     def templates(self):
         yield Path("activate.sh")
 
     def as_name(self, template):
         return template.stem
-
-
-def _convert_to_unix_line_endings(generated):
-    WINDOWS_LINE_ENDING = b"\r\n"
-    UNIX_LINE_ENDING = b"\n"
-
-    for file_path in generated:
-        with open(ensure_text(str(file_path)), "rb") as open_file:
-            content = open_file.read()
-
-        content = content.replace(WINDOWS_LINE_ENDING, UNIX_LINE_ENDING)
-
-        with open(ensure_text(str(file_path)), "wb") as open_file:
-            open_file.write(content)
