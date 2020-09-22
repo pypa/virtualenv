927aa92d0d3cceb44c541029a0673e8c90863e92
Dan Yeaw <dan@yeaw.me>
2020-09-21 22:29:55 -0400

    Fix activator path in cygwin and msys2
    Closes #1940. In cygwin and MSYS2, the path is in POSIX format.
    This PR converts the Windows path to POSIX format using a regex
    so that it is added to the bash activation script in the correct
    format.

--- tests/unit/activation/test_activation_support.py
+++ tests/unit/activation/test_activation_support.py
@@ -13,6 +13,8 @@ from virtualenv.activation import (
     PythonActivator,
 )
 from virtualenv.discovery.py_info import PythonInfo
+from virtualenv.info import IS_WIN
+from virtualenv.util.path import Path
 
 
 @pytest.mark.parametrize(
@@ -53,3 +55,32 @@ def test_activator_no_support_posix(mocker, activator_class):
     interpreter = mocker.Mock(spec=PythonInfo)
     interpreter.os = "posix"
     assert not activator.supports(interpreter)
+
+
+class Creator:
+    def __init__(self):
+        self.dest = "C:/tools/msys64/home"
+        self.env_name = "venv"
+        self.bin_dir = Path("C:/tools/msys64/home/bin")
+
+
+@pytest.mark.skipif(IS_WIN, reason="Github Actions ships with WSL bash")
+@pytest.mark.parametrize("activator_class", [BashActivator])
+def test_cygwin_msys2_path_conversion(mocker, activator_class):
+    mocker.patch("sysconfig.get_platform", return_value="mingw")
+    activator = activator_class(Namespace(prompt=None))
+    creator = Creator()
+    mocker.stub(creator.bin_dir.relative_to)
+    resource = activator.replacements(creator, "")
+    assert resource["__VIRTUAL_ENV__"] == "/c/tools/msys64/home"
+
+
+@pytest.mark.skipif(IS_WIN, reason="Github Actions ships with WSL bash")
+@pytest.mark.parametrize("activator_class", [BashActivator])
+def test_win_path_no_conversion(mocker, activator_class):
+    mocker.patch("sysconfig.get_platform", return_value="win-amd64")
+    activator = activator_class(Namespace(prompt=None))
+    creator = Creator()
+    mocker.stub(creator.bin_dir.relative_to)
+    resource = activator.replacements(creator, "")
+    assert resource["__VIRTUAL_ENV__"] == "C:/tools/msys64/home"
