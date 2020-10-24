601b813e45ab75f5154b8c89044ad95285c7e6ce
Bernát Gábor <bgabor8@bloomberg.net>
2020-10-25 00:01:04 +0100

    Bump pip to 20.2.4 (#1996)
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -6,33 +6,33 @@ from virtualenv.util.path import Path
 BUNDLE_FOLDER = Path(__file__).absolute().parent
 BUNDLE_SUPPORT = {
     "3.10": {
-        "pip": "pip-20.2.3-py2.py3-none-any.whl",
-        "setuptools": "setuptools-50.3.1-py3-none-any.whl",
+        "pip": "pip-20.2.4-py2.py3-none-any.whl",
+        "setuptools": "setuptools-50.3.2-py3-none-any.whl",
         "wheel": "wheel-0.35.1-py2.py3-none-any.whl",
     },
     "3.9": {
-        "pip": "pip-20.2.3-py2.py3-none-any.whl",
-        "setuptools": "setuptools-50.3.1-py3-none-any.whl",
+        "pip": "pip-20.2.4-py2.py3-none-any.whl",
+        "setuptools": "setuptools-50.3.2-py3-none-any.whl",
         "wheel": "wheel-0.35.1-py2.py3-none-any.whl",
     },
     "3.8": {
-        "pip": "pip-20.2.3-py2.py3-none-any.whl",
-        "setuptools": "setuptools-50.3.1-py3-none-any.whl",
+        "pip": "pip-20.2.4-py2.py3-none-any.whl",
+        "setuptools": "setuptools-50.3.2-py3-none-any.whl",
         "wheel": "wheel-0.35.1-py2.py3-none-any.whl",
     },
     "3.7": {
-        "pip": "pip-20.2.3-py2.py3-none-any.whl",
-        "setuptools": "setuptools-50.3.1-py3-none-any.whl",
+        "pip": "pip-20.2.4-py2.py3-none-any.whl",
+        "setuptools": "setuptools-50.3.2-py3-none-any.whl",
         "wheel": "wheel-0.35.1-py2.py3-none-any.whl",
     },
     "3.6": {
-        "pip": "pip-20.2.3-py2.py3-none-any.whl",
-        "setuptools": "setuptools-50.3.1-py3-none-any.whl",
+        "pip": "pip-20.2.4-py2.py3-none-any.whl",
+        "setuptools": "setuptools-50.3.2-py3-none-any.whl",
         "wheel": "wheel-0.35.1-py2.py3-none-any.whl",
     },
     "3.5": {
-        "pip": "pip-20.2.3-py2.py3-none-any.whl",
-        "setuptools": "setuptools-50.3.1-py3-none-any.whl",
+        "pip": "pip-20.2.4-py2.py3-none-any.whl",
+        "setuptools": "setuptools-50.3.2-py3-none-any.whl",
         "wheel": "wheel-0.35.1-py2.py3-none-any.whl",
     },
     "3.4": {
@@ -41,7 +41,7 @@ BUNDLE_SUPPORT = {
         "wheel": "wheel-0.33.6-py2.py3-none-any.whl",
     },
     "2.7": {
-        "pip": "pip-20.2.3-py2.py3-none-any.whl",
+        "pip": "pip-20.2.4-py2.py3-none-any.whl",
         "setuptools": "setuptools-44.1.1-py2.py3-none-any.whl",
         "wheel": "wheel-0.35.1-py2.py3-none-any.whl",
     },
