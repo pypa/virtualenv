5144e6a47422a5135e2a8ab0811c8875297ef254
Bernát Gábor <bgabor8@bloomberg.net>
2021-03-01 09:18:35 +0000

    Fix Pypy3 Windows stdlib path
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- src/virtualenv/create/via_global_ref/builtin/pypy/pypy3.py
+++ src/virtualenv/create/via_global_ref/builtin/pypy/pypy3.py
@@ -17,14 +17,6 @@ class PyPy3(PyPy, Python3Supports):
     def exe_stem(cls):
         return "pypy3"
 
-    @property
-    def stdlib(self):
-        """
-        PyPy3 seems to respect sysconfig only for the host python...
-        virtual environments purelib is instead lib/pythonx.y
-        """
-        return self.dest / "lib" / "python{}".format(self.interpreter.version_release_str) / "site-packages"
-
     @classmethod
     def exe_names(cls, interpreter):
         return super(PyPy3, cls).exe_names(interpreter) | {"pypy"}
@@ -32,6 +24,11 @@ class PyPy3(PyPy, Python3Supports):
 
 class PyPy3Posix(PyPy3, PosixSupports):
     """PyPy 2 on POSIX"""
+
+    @property
+    def stdlib(self):
+        """PyPy3 respects sysconfig only for the host python, virtual envs is instead lib/pythonx.y/site-packages"""
+        return self.dest / "lib" / "python{}".format(self.interpreter.version_release_str) / "site-packages"
 
     @classmethod
     def _shared_libs(cls):
@@ -52,6 +49,11 @@ class PyPy3Posix(PyPy3, PosixSupports):
 
 class Pypy3Windows(PyPy3, WindowsSupports):
     """PyPy 2 on Windows"""
+
+    @property
+    def stdlib(self):
+        """PyPy3 respects sysconfig only for the host python, virtual envs is instead Lib/site-packages"""
+        return self.dest / "Lib" / "site-packages"
 
     @property
     def bin_dir(self):
