9abbbfea13ae4639ae8f233a586ad54e40ebfda0
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-08-20 09:34:39 +0100

    using dest_folder for variables

--- src/virtualenv/activation/nushell/__init__.py
+++ src/virtualenv/activation/nushell/__init__.py
@@ -16,14 +16,12 @@ class NushellActivator(ViaTemplateActivator):
         # Due to nushell scoping, it isn't easy to create a function that will
         # deactivate the environment. For that reason a __DEACTIVATE_PATH__
         # replacement pointing to the deactivate.nu file is created
-        virtual_env = ensure_text(str(creator.dest))
-        bin_name = ensure_text(str(creator.bin_dir.relative_to(creator.dest)))
 
         return {
             "__VIRTUAL_PROMPT__": "" if self.flag_prompt is None else self.flag_prompt,
-            "__VIRTUAL_ENV__": virtual_env,
+            "__VIRTUAL_ENV__": ensure_text(str(creator.dest)),
             "__VIRTUAL_NAME__": creator.env_name,
-            "__BIN_NAME__": bin_name,
+            "__BIN_NAME__": ensure_text(str(creator.bin_dir.relative_to(creator.dest))),
             "__PATH_SEP__": ensure_text(os.pathsep),
-            "__DEACTIVATE_PATH__": str(Path(virtual_env, bin_name, "deactivate.nu")),
+            "__DEACTIVATE_PATH__": str(Path(dest_folder, "deactivate.nu")),
         }
