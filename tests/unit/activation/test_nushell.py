93a0cbc0316961c86b444981819aa852896b6cfb
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-08-31 13:56:54 +0100

    windows nu install with scoop

--- tests/unit/activation/test_nushell.py
+++ tests/unit/activation/test_nushell.py
@@ -6,7 +6,6 @@ from virtualenv.activation import NushellActivator
 from virtualenv.info import IS_WIN
 
 
-@pytest.mark.skipif(IS_WIN, reason="Github Actions ships with WSL bash")
 def test_nushell(activation_tester_class, activation_tester):
     class Nushell(activation_tester_class):
         def __init__(self, session):
