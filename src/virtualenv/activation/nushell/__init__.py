9704d2ca4768657e516642628b5de5b37bb85832
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-09-01 13:23:41 +0100

    using Path to join

--- src/virtualenv/activation/nushell/__init__.py
+++ src/virtualenv/activation/nushell/__init__.py
@@ -17,7 +17,6 @@ class NushellActivator(ViaTemplateActivator):
         # Due to nushell scoping, it isn't easy to create a function that will
         # deactivate the environment. For that reason a __DEACTIVATE_PATH__
         # replacement pointing to the deactivate.nu file is created
-        #
 
         return {
             "__VIRTUAL_PROMPT__": "" if self.flag_prompt is None else self.flag_prompt,
@@ -25,5 +24,5 @@ class NushellActivator(ViaTemplateActivator):
             "__VIRTUAL_NAME__": creator.env_name,
             "__BIN_NAME__": ensure_text(str(creator.bin_dir.relative_to(creator.dest))),
             "__PATH_SEP__": ensure_text(os.pathsep),
-            "__DEACTIVATE_PATH__": ensure_text(os.path.join(dest_folder, "deactivate.nu")),
+            "__DEACTIVATE_PATH__": ensure_text(str(Path(dest_folder) / Path("deactivate.nu"))),
         }
