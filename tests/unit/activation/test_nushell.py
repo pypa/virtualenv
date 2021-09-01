c340d7cab2f7ac81541637b45d7d0abd24b8d00a
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-09-01 09:51:57 +0100

    change name of cmd for windows

--- tests/unit/activation/test_nushell.py
+++ tests/unit/activation/test_nushell.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
 from virtualenv.activation import NushellActivator
+import sys
 
 
 def test_nushell(activation_tester_class, activation_tester):
     class Nushell(activation_tester_class):
         def __init__(self, session):
-            super(Nushell, self).__init__(NushellActivator, session, "nu", "activate.nu", "nu")
+            cmd = "c:\\program files\\nu\\bin\\nu.exe" if sys.platform == "win32" else "nu"
+            super(Nushell, self).__init__(NushellActivator, session, cmd, "activate.nu", "nu")
 
     activation_tester(Nushell)
