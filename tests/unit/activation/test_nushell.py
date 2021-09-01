9dd4828679cf55523484557c9bed7c21fdab0fd8
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-09-01 11:29:55 +0100

    linux line ending change

--- tests/unit/activation/test_nushell.py
+++ tests/unit/activation/test_nushell.py
@@ -1,13 +1,17 @@
 from __future__ import absolute_import, unicode_literals
 
-from virtualenv.activation import NushellActivator
 import sys
+
+from virtualenv.activation import NushellActivator
+from virtualenv.info import IS_WIN
 
 
 def test_nushell(activation_tester_class, activation_tester):
     class Nushell(activation_tester_class):
         def __init__(self, session):
-            cmd = "c:\\program files\\nu\\bin\\nu.exe" if sys.platform == "win32" else "nu"
+            cmd = "c:\\program files\\nu\\bin\\nu.exe" if IS_WIN else "nu"
             super(Nushell, self).__init__(NushellActivator, session, cmd, "activate.nu", "nu")
+
+            self.unix_line_ending = not IS_WIN
 
     activation_tester(Nushell)
