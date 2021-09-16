8f654a4fd507a4d8d4daadca0d20b1e6ffc8bf69
Fernando Herrera <fernando.j.herrera@gmail.com>
2021-09-16 09:20:58 +0100

    get path using shutil

--- tests/unit/activation/test_nushell.py
+++ tests/unit/activation/test_nushell.py
@@ -1,3 +1,4 @@
+import shutil
 from __future__ import absolute_import, unicode_literals
 
 from virtualenv.activation import NushellActivator
@@ -7,7 +8,10 @@ from virtualenv.info import IS_WIN
 def test_nushell(activation_tester_class, activation_tester):
     class Nushell(activation_tester_class):
         def __init__(self, session):
-            cmd = "c:\\program files\\nu\\bin\\nu.exe" if IS_WIN else "nu"
+            cmd = shutil.which("nu")
+            if cmd is None and IS_WIN:
+                cmd = "c:\\program files\\nu\\bin\\nu.exe"
+
             super(Nushell, self).__init__(NushellActivator, session, cmd, "activate.nu", "nu")
 
             self.unix_line_ending = not IS_WIN
