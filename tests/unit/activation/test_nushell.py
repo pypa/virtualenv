96795f95513416d06d19d92c6ebd0af09bb55dc4
Bernát Gábor <gaborjbernat@gmail.com>
2021-09-16 11:42:22 +0100

    Ammend command discovery
    We should only use deprected API if we must.
    
    Signed-off-by: Bernát Gábor <gaborjbernat@gmail.com>

--- tests/unit/activation/test_nushell.py
+++ tests/unit/activation/test_nushell.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
-from distutils.spawn import find_executable
+import sys
+
+if sys.version_info > (3,):
+    from shutil import which
+else:
+    from distutils.spawn import find_executable as which
+
 
 from virtualenv.activation import NushellActivator
 from virtualenv.info import IS_WIN
@@ -9,7 +15,7 @@ from virtualenv.info import IS_WIN
 def test_nushell(activation_tester_class, activation_tester):
     class Nushell(activation_tester_class):
         def __init__(self, session):
-            cmd = find_executable("nu")
+            cmd = which("nu")
             if cmd is None and IS_WIN:
                 cmd = "c:\\program files\\nu\\bin\\nu.exe"
 
