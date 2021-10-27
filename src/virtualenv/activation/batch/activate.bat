pypa/virtualenv

commit 35ec4416d2012cfcb7a5e638813f6d52e852f29f

Remove stray closing parenthesis in activate.bat (#2221)

src/virtualenv/activation/batch/activate.bat
@@ -17,7 +17,6 @@ if not defined VIRTUAL_ENV_DISABLE_PROMPT (
     if NOT DEFINED ENV_PROMPT (
         for %%d in ("%VIRTUAL_ENV%") do set "ENV_PROMPT=(%%~nxd) "
     )
-    )
     set "PROMPT=%ENV_PROMPT%%PROMPT%"
 )
 

commit 19c453ec8fe4407be165a6df0e60505bec7b52ed

Fix broken prompt set up by activate.bat (#2225)
Removing the stray closing parenthesis in activate.bat in commit
35ec441 broke the virtual environment prompt prefix in Windows cmd.
The reason for this was that the ill-formed closing parenthesis
somehow affected the variable expansion order.

This commit fixes the issue by not using a local ENV_PROMPT
variable to update the prompt.  (So an additional benefit of
this change is one less variable set in the environment.)

Fixes: https://github.com/pypa/virtualenv/issues/2225

src/virtualenv/activation/batch/activate.bat
@@ -13,11 +13,11 @@ if defined _OLD_VIRTUAL_PROMPT (
     )
 )
 if not defined VIRTUAL_ENV_DISABLE_PROMPT (
-    set "ENV_PROMPT=__VIRTUAL_PROMPT__"
-    if NOT DEFINED ENV_PROMPT (
-        for %%d in ("%VIRTUAL_ENV%") do set "ENV_PROMPT=(%%~nxd) "
+    if "__VIRTUAL_PROMPT__" NEQ "" (
+        set "PROMPT=__VIRTUAL_PROMPT__%PROMPT%"
+    ) else (
+        for %%d in ("%VIRTUAL_ENV%") do set "PROMPT=(%%~nxd) %PROMPT%"
     )
-    set "PROMPT=%ENV_PROMPT%%PROMPT%"
 )
 
 REM Don't use () to avoid problems with them in %PATH%