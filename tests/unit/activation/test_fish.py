8376963f197e9fef5f5e047e4cda7506da25d314
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-10-30 22:24:18 +0530

    Simplify fish command

--- tests/unit/activation/test_fish.py
+++ tests/unit/activation/test_fish.py
@@ -18,10 +18,6 @@ def test_fish(activation_tester_class, activation_tester, monkeypatch, tmp_path)
             super(Fish, self).__init__(FishActivator, session, "fish", "activate.fish", "fish")
 
         def print_prompt(self):
-            return self.python_cmd(
-                "import os;"
-                "venv_path = os.environ.get('VIRTUAL_ENV');"
-                r"""os.system("fish -c '. '\"'\"'{}/bin/activate.fish'\"'\"'; fish_prompt'".format(venv_path))"""
-            )
+            return "fish -c '. $VIRTUAL_ENV/bin/activate.fish; fish_prompt'"
 
     activation_tester(Fish)
