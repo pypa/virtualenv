3a7e44475faa8a9ff64432bea9f084dc12dfea06
Shayen Yatagama <53162318+shayeny@users.noreply.github.com>
2022-01-19 10:30:19 +0530

    Update activate.sh ' to "
    Change ' to " to support ' character in windows file system

--- src/virtualenv/activation/bash/activate.sh
+++ src/virtualenv/activation/bash/activate.sh
@@ -44,7 +44,7 @@ deactivate () {
 # unset irrelevant variables
 deactivate nondestructive
 
-VIRTUAL_ENV='__VIRTUAL_ENV__'
+VIRTUAL_ENV="__VIRTUAL_ENV__"
 if ([ "$OSTYPE" = "cygwin" ] || [ "$OSTYPE" = "msys" ]) && $(command -v cygpath &> /dev/null) ; then
     VIRTUAL_ENV=$(cygpath -u "$VIRTUAL_ENV")
 fi
