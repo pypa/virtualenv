92166a9e90b5949b84d85273dac682c54db134c1
Bernát Gábor <gaborjbernat@gmail.com>
2022-02-05 11:59:35 +0000

    Bump pip and setuptools (#2294)

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -6,28 +6,28 @@ from virtualenv.util.path import Path
 BUNDLE_FOLDER = Path(__file__).absolute().parent
 BUNDLE_SUPPORT = {
     "3.11": {
-        "pip": "pip-21.3.1-py3-none-any.whl",
-        "setuptools": "setuptools-60.5.0-py3-none-any.whl",
+        "pip": "pip-22.0.3-py3-none-any.whl",
+        "setuptools": "setuptools-60.7.1-py3-none-any.whl",
         "wheel": "wheel-0.37.1-py2.py3-none-any.whl",
     },
     "3.10": {
-        "pip": "pip-21.3.1-py3-none-any.whl",
-        "setuptools": "setuptools-60.5.0-py3-none-any.whl",
+        "pip": "pip-22.0.3-py3-none-any.whl",
+        "setuptools": "setuptools-60.7.1-py3-none-any.whl",
         "wheel": "wheel-0.37.1-py2.py3-none-any.whl",
     },
     "3.9": {
-        "pip": "pip-21.3.1-py3-none-any.whl",
-        "setuptools": "setuptools-60.5.0-py3-none-any.whl",
+        "pip": "pip-22.0.3-py3-none-any.whl",
+        "setuptools": "setuptools-60.7.1-py3-none-any.whl",
         "wheel": "wheel-0.37.1-py2.py3-none-any.whl",
     },
     "3.8": {
-        "pip": "pip-21.3.1-py3-none-any.whl",
-        "setuptools": "setuptools-60.5.0-py3-none-any.whl",
+        "pip": "pip-22.0.3-py3-none-any.whl",
+        "setuptools": "setuptools-60.7.1-py3-none-any.whl",
         "wheel": "wheel-0.37.1-py2.py3-none-any.whl",
     },
     "3.7": {
-        "pip": "pip-21.3.1-py3-none-any.whl",
-        "setuptools": "setuptools-60.5.0-py3-none-any.whl",
+        "pip": "pip-22.0.3-py3-none-any.whl",
+        "setuptools": "setuptools-60.7.1-py3-none-any.whl",
         "wheel": "wheel-0.37.1-py2.py3-none-any.whl",
     },
     "3.6": {
