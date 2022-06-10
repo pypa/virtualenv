04394b2dcfeb5c6bdecf405a46ab0e824bbe4e2f
reksarka <reksarka@gmail.com>
2022-06-10 23:51:35 +0300

    Skips one PY3 POSIX test in PY2 Windows

--- tests/unit/create/via_global_ref/builtin/pypy/test_pypy3.py
+++ tests/unit/create/via_global_ref/builtin/pypy/test_pypy3.py
@@ -5,6 +5,7 @@ from testing.helpers import contains_exe, contains_ref
 from testing.path import join as path
 
 from virtualenv.create.via_global_ref.builtin.pypy.pypy3 import PyPy3Posix
+from virtualenv.info import IS_WIN, PY2
 
 PYPY3_PATH = (
     "virtualenv.create.via_global_ref.builtin.pypy.common.Path",
@@ -12,6 +13,9 @@ PYPY3_PATH = (
 )
 
 
+# In `PyPy3Posix.sources()` `host_lib` will be broken in Python 2 for Windows,
+# so `py_file` will not be in sources.
+@pytest.mark.skipif(PY2 and IS_WIN, reason="Can't convert PosixPath")
 @pytest.mark.parametrize("py_info_name", ["portable_pypy38"])
 def test_portable_pypy3_virtualenvs_get_their_libs(py_info, mock_files, mock_pypy_libs):
     py_file = path(py_info.prefix, "lib/libgdbm.so.4")
