94422c3796885bbff23541b43f5d7b4de875e4bb
Paweł Szramowski <pawel.szramowski@gmail.com>
2022-09-07 00:11:38 +0200

    Do not turn echo off for subsequent commands in batch activators (#2411)

--- src/virtualenv/activation/batch/activate.bat
+++ src/virtualenv/activation/batch/activate.bat
@@ -1,39 +1,37 @@
-@echo off
+@set "VIRTUAL_ENV=__VIRTUAL_ENV__"
 
-set "VIRTUAL_ENV=__VIRTUAL_ENV__"
-
-if defined _OLD_VIRTUAL_PROMPT (
-    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+@if defined _OLD_VIRTUAL_PROMPT (
+    @set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
 ) else (
-    if not defined PROMPT (
-        set "PROMPT=$P$G"
+    @if not defined PROMPT (
+        @set "PROMPT=$P$G"
     )
-    if not defined VIRTUAL_ENV_DISABLE_PROMPT (
-        set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+    @if not defined VIRTUAL_ENV_DISABLE_PROMPT (
+        @set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
     )
 )
-if not defined VIRTUAL_ENV_DISABLE_PROMPT (
-    if "__VIRTUAL_PROMPT__" NEQ "" (
-        set "PROMPT=(__VIRTUAL_PROMPT__) %PROMPT%"
+@if not defined VIRTUAL_ENV_DISABLE_PROMPT (
+    @if "__VIRTUAL_PROMPT__" NEQ "" (
+        @set "PROMPT=(__VIRTUAL_PROMPT__) %PROMPT%"
     ) else (
-        for %%d in ("%VIRTUAL_ENV%") do set "PROMPT=(%%~nxd) %PROMPT%"
+        @for %%d in ("%VIRTUAL_ENV%") do @set "PROMPT=(%%~nxd) %PROMPT%"
     )
 )
 
-REM Don't use () to avoid problems with them in %PATH%
-if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
-    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+@REM Don't use () to avoid problems with them in %PATH%
+@if defined _OLD_VIRTUAL_PYTHONHOME @goto ENDIFVHOME
+    @set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
 :ENDIFVHOME
 
-set PYTHONHOME=
+@set PYTHONHOME=
 
-REM if defined _OLD_VIRTUAL_PATH (
-if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
-    set "PATH=%_OLD_VIRTUAL_PATH%"
+@REM if defined _OLD_VIRTUAL_PATH (
+@if not defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH1
+    @set "PATH=%_OLD_VIRTUAL_PATH%"
 :ENDIFVPATH1
-REM ) else (
-if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
-    set "_OLD_VIRTUAL_PATH=%PATH%"
+@REM ) else (
+@if defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH2
+    @set "_OLD_VIRTUAL_PATH=%PATH%"
 :ENDIFVPATH2
 
-set "PATH=%VIRTUAL_ENV%\__BIN_NAME__;%PATH%"
+@set "PATH=%VIRTUAL_ENV%\__BIN_NAME__;%PATH%"
