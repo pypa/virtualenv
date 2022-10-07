6bd7c994701f00013b4d48504a07f41839fd6554
Schaap <cybermonktitan@gmail.com>
2022-10-07 16:10:22 +0200

    Allow empty string as version for `find_compatible_in_house`
    The empty string is used in several locations to indicate the newest
    version. Interpret it as such instead of rejecting it as invalid.

--- tests/unit/seed/wheels/test_acquire_find_wheel.py
+++ tests/unit/seed/wheels/test_acquire_find_wheel.py
@@ -4,8 +4,14 @@ from virtualenv.seed.wheels.acquire import find_compatible_in_house
 from virtualenv.seed.wheels.embed import BUNDLE_FOLDER, MAX, get_embed_wheel
 
 
-def test_find_latest(for_py_version):
+def test_find_latest_none(for_py_version):
     result = find_compatible_in_house("setuptools", None, for_py_version, BUNDLE_FOLDER)
+    expected = get_embed_wheel("setuptools", for_py_version)
+    assert result.path == expected.path
+
+
+def test_find_latest_string(for_py_version):
+    result = find_compatible_in_house("setuptools", "", for_py_version, BUNDLE_FOLDER)
     expected = get_embed_wheel("setuptools", for_py_version)
     assert result.path == expected.path
 
