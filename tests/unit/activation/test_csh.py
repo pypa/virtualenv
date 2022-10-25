ae2594c5a822f62964f2ccef7832c26306bc66b1
Jakub Kulík <Kulikjak@gmail.com>
2022-10-25 07:00:20 +0200

    Fix test_csh when running with the original csh (#2418)

--- tests/unit/activation/test_csh.py
+++ tests/unit/activation/test_csh.py
@@ -7,6 +7,8 @@ def test_csh(activation_tester_class, activation_tester):
             super().__init__(CShellActivator, session, "csh", "activate.csh", "csh")
 
         def print_prompt(self):
-            return "echo 'source \"$VIRTUAL_ENV/bin/activate.csh\"; echo $prompt' | csh -i"
+            # Original csh doesn't print the last newline,
+            # breaking the test; hence the trailing echo.
+            return "echo 'source \"$VIRTUAL_ENV/bin/activate.csh\"; echo $prompt' | csh -i ; echo"
 
     activation_tester(Csh)
