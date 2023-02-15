97b9211feecb240f7d099d84610514c617a230af
faph <faph@users.noreply.github.com>
2023-02-15 09:13:44 +0000

    Revert wholesale removal of pre-filtering

--- src/virtualenv/discovery/windows/__init__.py
+++ src/virtualenv/discovery/windows/__init__.py
@@ -1,4 +1,5 @@
 from ..py_info import PythonInfo
+from ..py_spec import PythonSpec
 from .pep514 import discover_pythons
 
 
@@ -18,10 +19,15 @@ def propose_interpreters(spec, cache_dir, env):
     )
 
     for name, major, minor, arch, exe, _ in existing:
-        interpreter = Pep514PythonInfo.from_exe(exe, cache_dir, env=env, raise_on_error=False)
-        if interpreter is not None:
-            if interpreter.satisfies(spec, impl_must_match=True):
-                yield interpreter
+        # pre-filter
+        if name in ("PythonCore", "ContinuumAnalytics"):
+            name = "CPython"
+        registry_spec = PythonSpec(None, name, major, minor, None, arch, exe)
+        if registry_spec.satisfies(spec):
+            interpreter = Pep514PythonInfo.from_exe(exe, cache_dir, env=env, raise_on_error=False)
+            if interpreter is not None:
+                if interpreter.satisfies(spec, impl_must_match=True):
+                    yield interpreter
 
 
 __all__ = [
