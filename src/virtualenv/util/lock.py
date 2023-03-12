6756e7766361e5dc98cd47060530644f700daac6
Randy Döring <30527984+radoering@users.noreply.github.com>
2023-03-12 14:36:58 +0100

    Make `ReentrantFileLock` thread-safe and, thereby, fix race condition in `virtualenv.cli_run`

--- src/virtualenv/util/lock.py
+++ src/virtualenv/util/lock.py
@@ -23,13 +23,16 @@ class _CountedFileLock(FileLock):
         self.thread_safe = RLock()
 
     def acquire(self, timeout=None, poll_interval=0.05):
-        with self.thread_safe:
-            if self.count == 0:
-                super().acquire(timeout, poll_interval)
-            self.count += 1
+        if not self.thread_safe.acquire(timeout=-1 if timeout is None else timeout):
+            raise Timeout(self.lock_file)
+        if self.count == 0:
+            super().acquire(timeout, poll_interval)
+        self.count += 1
 
     def release(self, force=False):
         with self.thread_safe:
+            if self.count > 0:
+                self.thread_safe.release()
             if self.count == 1:
                 super().release(force=force)
             self.count = max(self.count - 1, 0)
