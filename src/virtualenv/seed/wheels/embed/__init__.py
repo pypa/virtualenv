41cec56660749510e0087a052c11acc02dda3cde
Bernát Gábor <gaborjbernat@gmail.com>
2023-04-19 12:57:24 -0700

    Bump embeded wheels (#2546)

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -5,34 +5,34 @@ from virtualenv.seed.wheels.util import Wheel
 BUNDLE_FOLDER = Path(__file__).absolute().parent
 BUNDLE_SUPPORT = {
     "3.12": {
-        "pip": "pip-23.0.1-py3-none-any.whl",
-        "setuptools": "setuptools-67.4.0-py3-none-any.whl",
-        "wheel": "wheel-0.38.4-py3-none-any.whl",
+        "pip": "pip-23.1-py3-none-any.whl",
+        "setuptools": "setuptools-67.6.1-py3-none-any.whl",
+        "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.11": {
-        "pip": "pip-23.0.1-py3-none-any.whl",
-        "setuptools": "setuptools-67.4.0-py3-none-any.whl",
-        "wheel": "wheel-0.38.4-py3-none-any.whl",
+        "pip": "pip-23.1-py3-none-any.whl",
+        "setuptools": "setuptools-67.6.1-py3-none-any.whl",
+        "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.10": {
-        "pip": "pip-23.0.1-py3-none-any.whl",
-        "setuptools": "setuptools-67.4.0-py3-none-any.whl",
-        "wheel": "wheel-0.38.4-py3-none-any.whl",
+        "pip": "pip-23.1-py3-none-any.whl",
+        "setuptools": "setuptools-67.6.1-py3-none-any.whl",
+        "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.9": {
-        "pip": "pip-23.0.1-py3-none-any.whl",
-        "setuptools": "setuptools-67.4.0-py3-none-any.whl",
-        "wheel": "wheel-0.38.4-py3-none-any.whl",
+        "pip": "pip-23.1-py3-none-any.whl",
+        "setuptools": "setuptools-67.6.1-py3-none-any.whl",
+        "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.8": {
-        "pip": "pip-23.0.1-py3-none-any.whl",
-        "setuptools": "setuptools-67.4.0-py3-none-any.whl",
-        "wheel": "wheel-0.38.4-py3-none-any.whl",
+        "pip": "pip-23.1-py3-none-any.whl",
+        "setuptools": "setuptools-67.6.1-py3-none-any.whl",
+        "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.7": {
-        "pip": "pip-23.0.1-py3-none-any.whl",
-        "setuptools": "setuptools-67.4.0-py3-none-any.whl",
-        "wheel": "wheel-0.38.4-py3-none-any.whl",
+        "pip": "pip-23.1-py3-none-any.whl",
+        "setuptools": "setuptools-67.6.1-py3-none-any.whl",
+        "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.6": {
         "pip": "pip-21.3.1-py3-none-any.whl",
