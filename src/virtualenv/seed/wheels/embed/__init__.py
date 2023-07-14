94f3cf581bc00eae81eca8bfd545fc3bec6d4bb6
Bernát Gábor <gaborjbernat@gmail.com>
2023-07-14 15:58:38 -0700

    Upgrade setuptools (#2607)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -8,32 +8,32 @@ BUNDLE_FOLDER = Path(__file__).absolute().parent
 BUNDLE_SUPPORT = {
     "3.7": {
         "pip": "pip-23.1.2-py3-none-any.whl",
-        "setuptools": "setuptools-67.8.0-py3-none-any.whl",
+        "setuptools": "setuptools-68.0.0-py3-none-any.whl",
         "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.8": {
         "pip": "pip-23.1.2-py3-none-any.whl",
-        "setuptools": "setuptools-67.8.0-py3-none-any.whl",
+        "setuptools": "setuptools-68.0.0-py3-none-any.whl",
         "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.9": {
         "pip": "pip-23.1.2-py3-none-any.whl",
-        "setuptools": "setuptools-67.8.0-py3-none-any.whl",
+        "setuptools": "setuptools-68.0.0-py3-none-any.whl",
         "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.10": {
         "pip": "pip-23.1.2-py3-none-any.whl",
-        "setuptools": "setuptools-67.8.0-py3-none-any.whl",
+        "setuptools": "setuptools-68.0.0-py3-none-any.whl",
         "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.11": {
         "pip": "pip-23.1.2-py3-none-any.whl",
-        "setuptools": "setuptools-67.8.0-py3-none-any.whl",
+        "setuptools": "setuptools-68.0.0-py3-none-any.whl",
         "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
     "3.12": {
         "pip": "pip-23.1.2-py3-none-any.whl",
-        "setuptools": "setuptools-67.8.0-py3-none-any.whl",
+        "setuptools": "setuptools-68.0.0-py3-none-any.whl",
         "wheel": "wheel-0.40.0-py3-none-any.whl",
     },
 }
