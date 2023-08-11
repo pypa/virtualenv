3e58856b1a7e7d334423af04e115cbe0eabf464b
Bernát Gábor <bgabor8@bloomberg.net>
2023-08-11 08:43:59 -0700

    Bump wheel
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from pathlib import Path
 
 from virtualenv.seed.wheels.util import Wheel
@@ -9,32 +7,32 @@ BUNDLE_SUPPORT = {
     "3.7": {
         "pip": "pip-23.2.1-py3-none-any.whl",
         "setuptools": "setuptools-68.0.0-py3-none-any.whl",
-        "wheel": "wheel-0.41.0-py3-none-any.whl",
+        "wheel": "wheel-0.41.1-py3-none-any.whl",
     },
     "3.8": {
         "pip": "pip-23.2.1-py3-none-any.whl",
         "setuptools": "setuptools-68.0.0-py3-none-any.whl",
-        "wheel": "wheel-0.41.0-py3-none-any.whl",
+        "wheel": "wheel-0.41.1-py3-none-any.whl",
     },
     "3.9": {
         "pip": "pip-23.2.1-py3-none-any.whl",
         "setuptools": "setuptools-68.0.0-py3-none-any.whl",
-        "wheel": "wheel-0.41.0-py3-none-any.whl",
+        "wheel": "wheel-0.41.1-py3-none-any.whl",
     },
     "3.10": {
         "pip": "pip-23.2.1-py3-none-any.whl",
         "setuptools": "setuptools-68.0.0-py3-none-any.whl",
-        "wheel": "wheel-0.41.0-py3-none-any.whl",
+        "wheel": "wheel-0.41.1-py3-none-any.whl",
     },
     "3.11": {
         "pip": "pip-23.2.1-py3-none-any.whl",
         "setuptools": "setuptools-68.0.0-py3-none-any.whl",
-        "wheel": "wheel-0.41.0-py3-none-any.whl",
+        "wheel": "wheel-0.41.1-py3-none-any.whl",
     },
     "3.12": {
         "pip": "pip-23.2.1-py3-none-any.whl",
         "setuptools": "setuptools-68.0.0-py3-none-any.whl",
-        "wheel": "wheel-0.41.0-py3-none-any.whl",
+        "wheel": "wheel-0.41.1-py3-none-any.whl",
     },
 }
 MAX = "3.7"
