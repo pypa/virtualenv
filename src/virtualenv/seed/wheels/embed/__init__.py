9e57a2a6c445185e07c49c1eec893d2e38a727db
Bernát Gábor <bgabor8@bloomberg.net>
2024-04-16 16:25:09 -0700

    Fix CI and bump wheels
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -13,33 +13,33 @@ BUNDLE_SUPPORT = {
     },
     "3.8": {
         "pip": "pip-24.0-py3-none-any.whl",
-        "setuptools": "setuptools-69.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.42.0-py3-none-any.whl",
+        "setuptools": "setuptools-69.5.1-py3-none-any.whl",
+        "wheel": "wheel-0.43.0-py3-none-any.whl",
     },
     "3.9": {
         "pip": "pip-24.0-py3-none-any.whl",
-        "setuptools": "setuptools-69.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.42.0-py3-none-any.whl",
+        "setuptools": "setuptools-69.5.1-py3-none-any.whl",
+        "wheel": "wheel-0.43.0-py3-none-any.whl",
     },
     "3.10": {
         "pip": "pip-24.0-py3-none-any.whl",
-        "setuptools": "setuptools-69.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.42.0-py3-none-any.whl",
+        "setuptools": "setuptools-69.5.1-py3-none-any.whl",
+        "wheel": "wheel-0.43.0-py3-none-any.whl",
     },
     "3.11": {
         "pip": "pip-24.0-py3-none-any.whl",
-        "setuptools": "setuptools-69.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.42.0-py3-none-any.whl",
+        "setuptools": "setuptools-69.5.1-py3-none-any.whl",
+        "wheel": "wheel-0.43.0-py3-none-any.whl",
     },
     "3.12": {
         "pip": "pip-24.0-py3-none-any.whl",
-        "setuptools": "setuptools-69.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.42.0-py3-none-any.whl",
+        "setuptools": "setuptools-69.5.1-py3-none-any.whl",
+        "wheel": "wheel-0.43.0-py3-none-any.whl",
     },
     "3.13": {
         "pip": "pip-24.0-py3-none-any.whl",
-        "setuptools": "setuptools-69.1.0-py3-none-any.whl",
-        "wheel": "wheel-0.42.0-py3-none-any.whl",
+        "setuptools": "setuptools-69.5.1-py3-none-any.whl",
+        "wheel": "wheel-0.43.0-py3-none-any.whl",
     },
 }
 MAX = "3.7"
