ada5bd74f94bc2b4705328567cfa8431fcf9b911
Joachim Fenkes <fenkes@de.ibm.com>
2024-05-08 15:47:16 +0200

    Fix bad return code in bash activation if hashing is disabled
    If hashing is disabled in bash, the `hash` command will return a nonzero
    return code. Since it is the last command in the script that return code
    will also be the "return code" of the source command, so if anyone uses
    
    source activate.sh || die horribly
    
    having hashing disabled will ruin their day.
    
    Fix this by overriding the return code of `hash` if it's bad.
    Add tests to verify a good return code and try with and without hashing.

--- tests/unit/activation/test_bash.py
+++ tests/unit/activation/test_bash.py
@@ -7,7 +7,8 @@ from virtualenv.info import IS_WIN
 
 
 @pytest.mark.skipif(IS_WIN, reason="Github Actions ships with WSL bash")
-def test_bash(raise_on_non_source_class, activation_tester):
+@pytest.mark.parametrize("hashing_enabled", [True, False])
+def test_bash(raise_on_non_source_class, hashing_enabled, activation_tester):
     class Bash(raise_on_non_source_class):
         def __init__(self, session) -> None:
             super().__init__(
@@ -18,6 +19,11 @@ def test_bash(raise_on_non_source_class, activation_tester):
                 "sh",
                 "You must source this script: $ source ",
             )
+            self.deactivate += " || exit 1"
+            self._invoke_script.append("-h" if hashing_enabled else "+h")
+
+        def activate_call(self, script):
+            return super().activate_call(script) + " || exit 1"
 
         def print_prompt(self):
             return self.print_os_env_var("PS1")
