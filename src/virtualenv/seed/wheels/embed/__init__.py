caeaaa8430234efe34fd126dc3b9a164928a8c12
Bernát Gábor <bgabor8@bloomberg.net>
2024-10-28 10:38:54 -0700

    Upgrade to pip 24.3
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -7,37 +7,37 @@ from virtualenv.seed.wheels.util import Wheel
 BUNDLE_FOLDER = Path(__file__).absolute().parent
 BUNDLE_SUPPORT = {
     "3.8": {
-        "pip": "pip-24.2-py3-none-any.whl",
+        "pip": "pip-24.3.1-py3-none-any.whl",
         "setuptools": "setuptools-75.2.0-py3-none-any.whl",
         "wheel": "wheel-0.44.0-py3-none-any.whl",
     },
     "3.9": {
-        "pip": "pip-24.2-py3-none-any.whl",
+        "pip": "pip-24.3.1-py3-none-any.whl",
         "setuptools": "setuptools-75.2.0-py3-none-any.whl",
         "wheel": "wheel-0.44.0-py3-none-any.whl",
     },
     "3.10": {
-        "pip": "pip-24.2-py3-none-any.whl",
+        "pip": "pip-24.3.1-py3-none-any.whl",
         "setuptools": "setuptools-75.2.0-py3-none-any.whl",
         "wheel": "wheel-0.44.0-py3-none-any.whl",
     },
     "3.11": {
-        "pip": "pip-24.2-py3-none-any.whl",
+        "pip": "pip-24.3.1-py3-none-any.whl",
         "setuptools": "setuptools-75.2.0-py3-none-any.whl",
         "wheel": "wheel-0.44.0-py3-none-any.whl",
     },
     "3.12": {
-        "pip": "pip-24.2-py3-none-any.whl",
+        "pip": "pip-24.3.1-py3-none-any.whl",
         "setuptools": "setuptools-75.2.0-py3-none-any.whl",
         "wheel": "wheel-0.44.0-py3-none-any.whl",
     },
     "3.13": {
-        "pip": "pip-24.2-py3-none-any.whl",
+        "pip": "pip-24.3.1-py3-none-any.whl",
         "setuptools": "setuptools-75.2.0-py3-none-any.whl",
         "wheel": "wheel-0.44.0-py3-none-any.whl",
     },
     "3.14": {
-        "pip": "pip-24.2-py3-none-any.whl",
+        "pip": "pip-24.3.1-py3-none-any.whl",
         "setuptools": "setuptools-75.2.0-py3-none-any.whl",
         "wheel": "wheel-0.44.0-py3-none-any.whl",
     },
