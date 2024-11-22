344bb95acb288257c77ba4309f90f04b2f1590de
Wiktor Brozynski <wbrozyn@ra.rockwell.com>
2024-11-22 12:57:26 +0100

    Try a test for output

--- tests/unit/activation/test_batch.py
+++ tests/unit/activation/test_batch.py
@@ -33,3 +33,43 @@ def test_batch(activation_tester_class, activation_tester, tmp_path):
             return 'echo "%PROMPT%"'
 
     activation_tester(Batch)
+
+
+@pytest.mark.usefixtures("activation_python")
+def test_batch_output(activation_tester_class, activation_tester, tmp_path):
+    version_script = tmp_path / "version.bat"
+    version_script.write_text("ver", encoding="utf-8")
+
+    class Batch(activation_tester_class):
+        def __init__(self, session) -> None:
+            super().__init__(BatchActivator, session, None, "activate.bat", "bat")
+            self._version_cmd = [str(version_script)]
+            self._invoke_script = []
+            self.deactivate = "call deactivate"
+            self.activate_cmd = "call"
+            self.pydoc_call = f"call {self.pydoc_call}"
+            self.unix_line_ending = False
+
+        def _get_test_lines(self, activate_script):
+            intermediary_script_path = str(tmp_path / "intermediary.bat")
+            return [
+                "@echo on",
+                f"echo @call {activate_script} > {intermediary_script_path}",
+                f"echo @echo >> {intermediary_script_path}",
+                f"echo @deactivate >> {intermediary_script_path}",
+                f"call {intermediary_script_path}"
+            ]
+
+        def assert_output(self, out, raw, tmp_path):
+            assert out[0] == "ECHO is on.", raw
+
+        def quote(self, s):
+            if '"' in s or " " in s:
+                text = s.replace('"', r"\"")
+                return f'"{text}"'
+            return s
+
+        def print_prompt(self):
+            return 'echo "%PROMPT%"'
+
+    activation_tester(Batch)
