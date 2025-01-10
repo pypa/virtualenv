cadb3bcc883ea5e225673bff25fb6d214b8c1421
Robsdedude <rouven.bauer@neo4j.com>
2025-01-10 16:48:48 +0100

    CI: hack to get tox to pick up python3.13t

--- tasks/pick_tox_env.py
+++ tasks/pick_tox_env.py
@@ -8,5 +8,7 @@ py = sys.argv[1]
 if py.startswith("brew@"):
     py = py[len("brew@") :]
 env = f"TOXENV={py}"
+if len(sys.argv) > 2:  # noqa: PLR2004
+    env += f"\nTOX_BASEPYTHON={sys.argv[2]}"
 with Path(os.environ["GITHUB_ENV"]).open("ta", encoding="utf-8") as file_handler:
     file_handler.write(env)
