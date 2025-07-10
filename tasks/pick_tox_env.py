db914d30e0d8bf84ab5b91cc29f10df7421429ed
Bernát Gábor <bgabor8@bloomberg.net>
2025-07-10 09:34:58 -0700

    Fix the CI
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tasks/pick_tox_env.py
+++ tasks/pick_tox_env.py
@@ -5,12 +5,20 @@ import sys
 from pathlib import Path
 
 py = sys.argv[1]
+env_lines = []
+
 if py.startswith("brew@"):
-    py = py[len("brew@") :]
+    brew_version = py[len("brew@") :]
+    env_lines.append(f"TOX_DISCOVER=/opt/homebrew/bin/python{brew_version}")
+    py = brew_version
+
 if py.startswith("graalpy-"):
     py = "graalpy"
-env = f"TOXENV={py}"
-if len(sys.argv) > 2:  # noqa: PLR2004
-    env += f"\nTOX_BASEPYTHON={sys.argv[2]}"
-with Path(os.environ["GITHUB_ENV"]).open("ta", encoding="utf-8") as file_handler:
+
+env_lines.append(f"TOXENV={py}")
+env = "\n".join(env_lines) + "\n"
+
+with Path(os.environ["GITHUB_ENV"]).open("a", encoding="utf-8") as file_handler:
     file_handler.write(env)
+
+print(f"Written to GITHUB_ENV:\n{env}")
