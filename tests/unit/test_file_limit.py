b9f29017677337ace6a39e4bdc87078df5ffb5e1
Emre Şafak <3928300+esafak@users.noreply.github.com>
2025-08-05 10:57:31 -0400

    Enhance file limit test for JIT implementations
    * Add exception handling for `resource.setrlimit` to skip tests on implementations that don't support it (e.g., PyPy, GraalPy).
    * Adjust the number of opened file descriptors to account for JIT
      implementations which may consume more initially.
    * Add specific checks for `OSError` with `errno.EMFILE` and the "Too many open files" message for JIT environments.
    
    Signed-off-by: Emre Şafak <3928300+esafak@users.noreply.github.com>

--- tests/unit/test_file_limit.py
+++ tests/unit/test_file_limit.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import errno
 import os
 import sys
 
 import pytest
+from virtualenv.info import IMPLEMENTATION
 
 from virtualenv.run import cli_run
 
@@ -22,11 +24,19 @@ def test_too_many_open_files(tmp_path):
         resource.setrlimit(resource.RLIMIT_NOFILE, (32, hard_limit))
     except ValueError:
         pytest.skip("could not lower the soft limit for open files")
+    except AttributeError as exc:   # pypy, graalpy
+        if "module 'resource' has no attribute 'setrlimit'" in str(exc):
+            pytest.skip(f"{IMPLEMENTATION} does not support resource.setrlimit")
 
     # Keep some file descriptors open to make it easier to trigger the error
     fds = []
     try:
-        fds.extend(os.open(os.devnull, os.O_RDONLY) for _ in range(20))
+        # JIT implementations use more file descriptors up front so we can run out early
+        try:
+            fds.extend(os.open(os.devnull, os.O_RDONLY) for _ in range(20))
+        except OSError as jit_exceptions:  # pypy, graalpy
+            assert jit_exceptions.errno == errno.EMFILE
+            assert "Too many open files" in str(jit_exceptions)
 
         expected_exceptions = SystemExit, OSError, RuntimeError
         with pytest.raises(expected_exceptions) as too_many_open_files_exc:
