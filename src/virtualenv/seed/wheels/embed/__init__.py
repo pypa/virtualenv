6951cae08afbbb04464ad043dc73bde1fae04293
Rahul Devikar <rahuldevikar5512@gmail.com>
2026-01-01 20:14:59 -0800

    Fix CVE-2025-47273

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -8,7 +8,7 @@ BUNDLE_FOLDER = Path(__file__).absolute().parent
 BUNDLE_SUPPORT = {
     "3.8": {
         "pip": "pip-25.0.1-py3-none-any.whl",
-        "setuptools": "setuptools-75.3.2-py3-none-any.whl",
+        "setuptools": "setuptools-80.9.0-py3-none-any.whl",
         "wheel": "wheel-0.45.1-py3-none-any.whl",
     },
     "3.9": {
