d27a884e8842409a7613360eac54531db777fbbf
rahuldevikar <rahuldevikar5512@gmail.com>
2026-02-09 13:36:12 -0800

    Upgrade embedded dependencies (#3026)

--- src/virtualenv/seed/wheels/embed/__init__.py
+++ src/virtualenv/seed/wheels/embed/__init__.py
@@ -8,36 +8,36 @@ BUNDLE_FOLDER = Path(__file__).absolute().parent
 BUNDLE_SUPPORT = {
     "3.8": {
         "pip": "pip-25.0.1-py3-none-any.whl",
-        "setuptools": "setuptools-75.3.2-py3-none-any.whl",
+        "setuptools": "setuptools-75.3.4-py3-none-any.whl",
         "wheel": "wheel-0.45.1-py3-none-any.whl",
     },
     "3.9": {
-        "pip": "pip-25.3-py3-none-any.whl",
-        "setuptools": "setuptools-80.9.0-py3-none-any.whl",
+        "pip": "pip-26.0.1-py3-none-any.whl",
+        "setuptools": "setuptools-82.0.0-py3-none-any.whl",
     },
     "3.10": {
-        "pip": "pip-25.3-py3-none-any.whl",
-        "setuptools": "setuptools-80.9.0-py3-none-any.whl",
+        "pip": "pip-26.0.1-py3-none-any.whl",
+        "setuptools": "setuptools-82.0.0-py3-none-any.whl",
     },
     "3.11": {
-        "pip": "pip-25.3-py3-none-any.whl",
-        "setuptools": "setuptools-80.9.0-py3-none-any.whl",
+        "pip": "pip-26.0.1-py3-none-any.whl",
+        "setuptools": "setuptools-82.0.0-py3-none-any.whl",
     },
     "3.12": {
-        "pip": "pip-25.3-py3-none-any.whl",
-        "setuptools": "setuptools-80.9.0-py3-none-any.whl",
+        "pip": "pip-26.0.1-py3-none-any.whl",
+        "setuptools": "setuptools-82.0.0-py3-none-any.whl",
     },
     "3.13": {
-        "pip": "pip-25.3-py3-none-any.whl",
-        "setuptools": "setuptools-80.9.0-py3-none-any.whl",
+        "pip": "pip-26.0.1-py3-none-any.whl",
+        "setuptools": "setuptools-82.0.0-py3-none-any.whl",
     },
     "3.14": {
-        "pip": "pip-25.3-py3-none-any.whl",
-        "setuptools": "setuptools-80.9.0-py3-none-any.whl",
+        "pip": "pip-26.0.1-py3-none-any.whl",
+        "setuptools": "setuptools-82.0.0-py3-none-any.whl",
     },
     "3.15": {
-        "pip": "pip-25.3-py3-none-any.whl",
-        "setuptools": "setuptools-80.9.0-py3-none-any.whl",
+        "pip": "pip-26.0.1-py3-none-any.whl",
+        "setuptools": "setuptools-82.0.0-py3-none-any.whl",
     },
 }
 MAX = "3.8"
