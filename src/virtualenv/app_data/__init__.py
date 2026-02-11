d2585f87b2bb8bcf62063b52b82ce8580dd32ff1
Rahul Devikar <rahuldevikar5512@gmail.com>
2026-02-11 15:50:33 -0800

    Use user_cache_dir for app data with auto-migration from old location

--- src/virtualenv/app_data/__init__.py
+++ src/virtualenv/app_data/__init__.py
@@ -4,8 +4,9 @@ from __future__ import annotations
 
 import logging
 import os
+import shutil
 
-from platformdirs import user_data_dir
+from platformdirs import user_cache_dir, user_data_dir
 
 from .na import AppDataDisabled
 from .read_only import ReadOnlyAppData
@@ -19,7 +20,24 @@ def _default_app_data_dir(env):
     key = "VIRTUALENV_OVERRIDE_APP_DATA"
     if key in env:
         return env[key]
-    return user_data_dir(appname="virtualenv", appauthor="pypa")
+    return _cache_dir_with_migration()
+
+
+def _cache_dir_with_migration():
+    new_dir = user_cache_dir(appname="virtualenv", appauthor="pypa")
+    old_dir = user_data_dir(appname="virtualenv", appauthor="pypa")
+    if new_dir == old_dir:
+        return new_dir
+    if os.path.isdir(old_dir) and not os.path.isdir(new_dir):
+        LOGGER.info("migrating app data from %s to %s", old_dir, new_dir)
+        try:
+            shutil.move(old_dir, new_dir)
+        except OSError as exception:
+            LOGGER.warning(
+                "could not migrate app data from %s to %s: %r, using old location", old_dir, new_dir, exception
+            )
+            return old_dir
+    return new_dir
 
 
 def make_app_data(folder, **kwargs):
